b8886bca7db309e3b0b7ae7bbbcf62058b38fdd8
Jingbei Li <i@jingbei.li>
2016-09-13 23:27:29 +0800

    addpkg:	vim-youcompleteme-git

---
+++ vim-youcompleteme-git/lilac.py
@@ -0,0 +1,14 @@
+from lilaclib import *
+
+build_prefix = 'extra-x86_64'
+
+def pre_build():
+    run_cmd(['wget', '-O', 'PKGBUILD', 'https://raw.githubusercontent.com/archlinuxcn/repo/master/vim-youcompleteme-git/PKGBUILD'])
+    vcs_update()
+
+def post_build():
+  git_add_files('PKGBUILD')
+  git_commit()
+
+if __name__ == '__main__':
+  single_main(build_prefix)
