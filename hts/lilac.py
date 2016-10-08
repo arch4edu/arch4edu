64f1307641ed1dab3b4073758d2462b24e98d7df
Jingbei Li <i@jingbei.li>
2016-10-08 19:22:47 +0800

    fix hts/lilac.py

--- hts/lilac.py
+++ hts/lilac.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
-#
-# This file is the most simple lilac.py file,
-# and it suits for most packages in AUR.
-#
 
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
+build_prefix = 'arch4edu-x86_64'
+
+def pre_build():
+  aur_pre_build()
+
+  for line in edit_file('PKGBUILD'):
+    if 'file://' in line:
+        print(line.replace('file://',''))
+    else:
+        print(line)
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
-  single_main()
+  single_main(build_prefix)
