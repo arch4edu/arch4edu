e8f09ab11a63a8eb69a199996c3607ded5c7a7da
Jingbei Li <i@jingbei.li>
2016-10-16 20:25:08 +0800

    addpkg:	torch7-warp-ctc

---
+++ torch7-warp-ctc/lilac.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from lilaclib import *
+
+build_prefix = 'arch4edu-x86_64'
+
+def pre_build():
+  aur_pre_build()
+
+  for line in edit_file('PKGBUILD'):
+    if 'makedepends' in line:
+        print(line.replace(')',' "torch7-cutorch-git")'))
+    else:
+        print(line)
+
+post_build = aur_post_build
+
+if __name__ == '__main__':
+  single_main(build_prefix)
