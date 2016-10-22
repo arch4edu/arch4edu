a0c08d94de323e6e5ec2783471d7301c5c855ed3
Jingbei Li <i@jingbei.li>
2016-10-22 16:07:35 +0800

    merge all tensorflow packages into one

---
+++ python-tensorflow-git/lilac.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from lilaclib import *
+
+build_prefix = 'arch4edu-x86_64'
+
+def pre_build():
+  aur_pre_build()
+
+  for line in edit_file('PKGBUILD'):
+    if 'pkgdesc' in line:
+        print(line.replace('."','.(added support for NVIDIA GPUs with compute capability 5.2,6.1)"'))
+    elif 'makedepends' in line:
+        print(line.replace(')',' "cuda" "cudnn")'))
+    elif 'TF_CUDA_COMPUTE_CAPABILITIES' in line:
+        print('    export TF_CUDA_COMPUTE_CAPABILITIES="5.2,6.1"')
+    elif './configure' in line:
+        print('  export TF_NEED_GCP=0')
+        print('  export TF_NEED_HDFS=0')
+        print('  export CC=gcc-5')
+        print('  echo -e "\\n\\n\\n"|'+line)
+    else:
+        print(line)
+
+post_build = aur_post_build
+
+if __name__ == '__main__':
+  single_main(build_prefix)
