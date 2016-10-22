2da430427aaa4bb1da84b31d8c593d9c59a2a4f6
Jingbei Li <i@jingbei.li>
2016-10-22 17:10:21 +0800

    switch the mirror site for eclipse packages

--- eclipse-wtp/lilac.py
+++ eclipse-wtp/lilac.py
@@ -4,7 +4,15 @@ from lilaclib import *
 
 build_prefix = 'arch4edu-x86_64'
 depends=['eclipse-emf', 'eclipse-gef']
-pre_build = aur_pre_build
+
+def pre_build():
+  aur_pre_build()
+  for line in edit_file('PKGBUILD'):
+    if '_mirror=' in line:
+      print('_mirror=http://mirrors.ustc.edu.cn/eclipse')
+    else:
+      print(line)
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
