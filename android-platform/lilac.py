c96671b89237cd653217c40df92d4accfde39869
Jingbei Li <i@jingbei.li>
2016-11-01 22:27:06 +0800

    addpkg: android-platform

---
+++ android-platform/lilac.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from lilaclib import *
+
+build_prefix = 'extra-x86_64'
+depends = ['android-sdk', 'android-sdk-platform-tools']
+def pre_build():
+    aur_pre_build()
+    import os
+    os.environ['https_proxy']='127.0.0.1:8123'
+post_build = aur_post_build
+
+if __name__ == '__main__':
+  single_main()
