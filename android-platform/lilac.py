9b4656f195db30738ad6987a5166a01018b0c350
Jingbei Li <i@jingbei.li>
2016-11-02 02:29:02 +0800

    fix android-platform/lilac.py

--- android-platform/lilac.py
+++ android-platform/lilac.py
@@ -2,7 +2,7 @@
 
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
+build_prefix = 'arch4edu-x86_64'
 depends = ['android-sdk', 'android-sdk-platform-tools']
 def pre_build():
     aur_pre_build()
