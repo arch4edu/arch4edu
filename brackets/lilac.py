e72fdd6b083a749deb1c4b2b2bfee033168d359c
Jingbei Li <i@jingbei.li>
2016-12-01 06:29:00 +0800

    fix brackets/lilac.py

--- brackets/lilac.py
+++ brackets/lilac.py
@@ -4,8 +4,19 @@ from lilaclib import *
 
 build_prefix = 'arch4edu-x86_64'
 depends=['gyp-git']
-pre_build = aur_pre_build
+def pre_build():
+    aur_pre_build()
+
+    for line in edit_file('PKGBUILD'):
+        if line[0:9]=='depends=(':
+            print(line.replace(')',' nss)'))
+        elif 'makedepends=(' in line:
+            print(line.replace(')',' "gtk2")'))
+        else:
+            print(line)
+    import os
+    os.environ['http_proxy']='127.0.0.1:8123'
 post_build = aur_post_build
 
 if __name__ == '__main__':
-  single_main()
+  single_main(build_prefix)
