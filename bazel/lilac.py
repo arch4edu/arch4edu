e558d43d123a887cb0e98ef6da23be54df1eeed6
Jingbei Li <i@jingbei.li>
2016-12-05 13:56:14 +0800

    update bazel/lilac.py

--- bazel/lilac.py
+++ bazel/lilac.py
@@ -2,9 +2,9 @@
 
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
+build_prefix = 'multilib'
 pre_build = aur_pre_build
 post_build = aur_post_build
 
 if __name__ == '__main__':
-  single_main()
+  single_main(build_prefix)
