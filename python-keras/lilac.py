95a4714c0f0b6bcdbeb0f3ec64760003d0d80897
Jingbei Li <i@jingbei.li>
2017-01-27 19:46:37 +0800

    trigger rebuild for python-keras

--- python-keras/lilac.py
+++ python-keras/lilac.py
@@ -1,17 +1,8 @@
 #!/usr/bin/env python3
-
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-
-def pre_build():
-  aur_pre_build()
-  for line in edit_file('PKGBUILD'):
-    if 'pkgrel=' in line:
-        print(line.replace('"',''))
-    else:
-        print(line)
-
+pre_build = aur_pre_build
 post_build = aur_post_build
 
 if __name__ == '__main__':
