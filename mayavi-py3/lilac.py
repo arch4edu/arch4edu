4402a97eece03e3f9d2a49f99cabfde5d47455c0
Jingbei Li <i@jingbei.li>
2017-02-13 16:20:53 +0800

    fix mayavi-py3/lilac.py

--- mayavi-py3/lilac.py
+++ mayavi-py3/lilac.py
@@ -4,7 +4,20 @@ from lilaclib import *
 
 build_prefix = 'arch4edu-x86_64'
 depends = ['vtk-py3-qt4']
-pre_build = aur_pre_build
+
+def pre_build():
+  aur_pre_build()
+
+  for line in edit_file('PKGBUILD'):
+    if 'makedepends=(' in line:
+        print(line.replace(')',' "openmpi" "gdal" "unixodbc" "tk" "ffmpeg" "jsoncpp" "gcc-libs")'))
+    elif 'patch -p1 < ../python35-ast.patch' in line:
+        print(line)
+        print('   #To strip non-ascii characters from docs')
+        print('   sed "166s/doc/doc.encode(\'ascii\',errors=\'ignore\').decode(\'ascii\')/" -i tvtk/indenter.py')
+    else:
+        print(line)
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
