5703fbb63793e7baff0f108fb8c0c6dbf87a1600
Jingbei Li <i@jingbei.li>
2017-02-18 16:29:02 +0800

    setup JAVA_HOME in scilab/lilac.py

--- scilab/lilac.py
+++ scilab/lilac.py
@@ -3,8 +3,13 @@
 from lilaclib import *
 
 build_prefix = 'arch4edu-x86_64'
-depends=['fop-hyph', 'java-flexdock', 'java-qdox', 'java-skinlf', 'java-testng', 'javahelp2', 'jeuclid-core', 'jgoodies-looks', 'jgraphx', 'jlatexmath-fop', 'jogl', 'jrosetta', 'saxon-he']
-pre_build = aur_pre_build
+depends=['apache-lucene', 'fop-hyph', 'java-flexdock', 'java-qdox', 'java-skinlf', 'java-testng', 'javahelp2', 'jeuclid-core', 'jgoodies-looks', 'jgraphx', 'jlatexmath-fop', 'jogl', 'jrosetta', 'saxon-he']
+
+def pre_build():
+    aur_pre_build()
+    import os
+    os.environ['JAVA_HOME']='/usr/lib/jvm/default'
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
