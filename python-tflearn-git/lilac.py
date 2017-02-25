253dbb70abe29253dd14d60c734e5512605154bd
Jingbei Li <i@jingbei.li>
2017-02-25 22:53:52 +0800

    build python-tflearn-git/lilac.py without tensorflow

--- python-tflearn-git/lilac.py
+++ python-tflearn-git/lilac.py
@@ -2,9 +2,8 @@
 from lilaclib import *
 
 build_prefix = 'arch4edu-x86_64'
-depends=['python-tensorflow-git']
 pre_build = aur_pre_build
 post_build = aur_post_build
 
 if __name__ == '__main__':
-  single_main()
+  single_main(build_prefix)
