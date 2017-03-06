d994a5b1d5cac13e613bbd0f9789beb432f89696
Jingbei Li <i@jingbei.li>
2017-03-06 11:22:12 +0800

    fix mavproxy-git/lilac.py

--- mavproxy-git/lilac.py
+++ mavproxy-git/lilac.py
@@ -2,9 +2,18 @@
 
 from lilaclib import *
 
-build_prefix = 'arch4edu-x86_64'
-depends=['python2-pymavlink-git']
-pre_build = aur_pre_build
+build_prefix = 'extra-x86_64'
+depends = ["python2-pymavlink-git"]
+
+def pre_build():
+  aur_pre_build()
+
+  for line in edit_file('PKGBUILD'):
+    if 'makedepends' in line:
+        print(line.replace(')',' "git")'))
+    else:
+        print(line)
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
