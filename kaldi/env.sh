3d3fb06c762b664c4cbf6f78293206cab4ee84bf
calil <calil@jingbei.li>
2017-03-29 00:24:22 +0800

    auto update for package kaldi

---
+++ kaldi/env.sh
@@ -0,0 +1,7 @@
+export IRSTLM=/opt/kaldi/tools/irstlm
+export PATH=${PATH}:${IRSTLM}/bin
+export PATH=${PATH}:/opt/kaldi/tools/kaldi_lm
+export LIBLBFGS=/opt/kaldi/tools/liblbfgs-1.10
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${LIBLBFGS}/lib/.libs
+export SRILM=/opt/kaldi/tools/srilm
+export PATH=${PATH}:${SRILM}/bin:${SRILM}/bin/i686-m64
