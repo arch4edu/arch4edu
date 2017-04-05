ecf0c8612758216ba02ee93ea01c8c960d038886
Jingbei Li <i@jingbei.li>
2017-04-05 15:45:55 +0800

    addpkg:	torch7-nnx-git

--- brackets/lilac.py
+++ brackets/lilac.py
@@ -1,22 +1,16 @@
 #!/usr/bin/env python3
-
+import os
 from lilaclib import *
 
 build_prefix = 'arch4edu-x86_64'
-depends=['gyp-git']
+
 def pre_build():
     aur_pre_build()
-
-    for line in edit_file('PKGBUILD'):
-        if line[0:9]=='depends=(':
-            print(line.replace(')',' nss)'))
-        elif 'makedepends=(' in line:
-            print(line.replace(')',' "gtk2")'))
-        else:
-            print(line)
-    import os
     os.environ['http_proxy']='127.0.0.1:8123'
-post_build = aur_post_build
+
+def post_build():
+    aur_post_build()
+    del os.environ['http_proxy']
 
 if __name__ == '__main__':
   single_main(build_prefix)
