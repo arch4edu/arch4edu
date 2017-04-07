f143a6c9a506b96abf940da1dc91c741016aeec3
Jingbei Li <i@jingbei.li>
2017-04-07 09:59:06 +0800

    addpkg:	torch7-rnn-git

---
+++ torch7-rnn-git/lilac.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from lilaclib import *
+
+build_prefix = 'arch4edu-x86_64'
+depends=['torch7-cunnx-git', 'torch7-dpnn-git', 'torch7-git', 'torch7-nn-git', 'torch7-nnx-git', 'torch7-torchx-git']
+pre_build = aur_pre_build
+post_build = aur_post_build
+
+if __name__ == '__main__':
+  single_main(build_prefix)
