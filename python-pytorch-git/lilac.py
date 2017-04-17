713e5868a3218cda706454550b6a3dc1248ec793
Jingbei Li <i@jingbei.li>
2017-04-17 16:08:27 +0800

    fix python-pytorch-git/lilac.py

--- python-pytorch-git/lilac.py
+++ python-pytorch-git/lilac.py
@@ -3,7 +3,18 @@
 from lilaclib import *
 
 build_prefix = 'arch4edu-x86_64'
-pre_build = aur_pre_build
+
+def pre_build():
+    aur_pre_build()
+
+    for line in edit_file('PKGBUILD'):
+        if 'depennds=(' in line:
+            print(line.replace('depennds','depends'))
+        elif 'makedepends=(' in line:
+            print(line.replace(')', ' "git" "python-yaml" "python2-yaml")'))
+        else:
+            print(line)
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
