17200e98c5c7868ab27948245aa9b10ad8ab6be1
Jingbei Li <i@jingbei.li>
2017-04-30 23:51:53 +0800

    update brackets/lilac.py

--- brackets/lilac.py
+++ brackets/lilac.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
-import os
 from lilaclib import *
-
-build_prefix = 'arch4edu-x86_64'
+import os
+build_prefix = 'extra-x86_64'
 
 def pre_build():
     aur_pre_build()
-    os.environ['http_proxy']='127.0.0.1:8123'
+    os.environ['https_proxy']='127.0.0.1:8123'
 
 def post_build():
     aur_post_build()
-    del os.environ['http_proxy']
+    del os.environ['https_proxy']
 
 if __name__ == '__main__':
   single_main(build_prefix)
