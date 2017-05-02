7a4605891845d03b7c502469ab9c44caf2d00669
Jingbei Li <i@jingbei.li>
2017-05-03 00:17:33 +0800

    set up proxy for brackets

--- brackets/lilac.py
+++ brackets/lilac.py
@@ -5,10 +5,12 @@ build_prefix = 'extra-x86_64'
 
 def pre_build():
     aur_pre_build()
+    os.environ['http_proxy']='127.0.0.1:8123'
     os.environ['https_proxy']='127.0.0.1:8123'
 
 def post_build():
     aur_post_build()
+    del os.environ['http_proxy']
     del os.environ['https_proxy']
 
 if __name__ == '__main__':
