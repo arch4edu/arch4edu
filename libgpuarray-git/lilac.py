222a215348fbd68e284fb5159f43e0191af5ecd0
Jingbei Li <i@jingbei.li>
2017-09-04 15:54:50 +0800

    added missing makedepends for libgpuarray-git

--- libgpuarray-git/lilac.py
+++ libgpuarray-git/lilac.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
-
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
+
+def pre_build():
+    aur_pre_build()
+
+    for line in edit_file('PKGBUILD'):
+        if 'makedepends=(' in line:
+            print(line.replace(")"," 'python')"))
+        else:
+            print(line)
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
