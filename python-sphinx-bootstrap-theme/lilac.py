7c869b8535e50a8601236b32ce04b7df65775727
Jingbei Li <i@jingbei.li>
2017-10-03 16:17:25 +0800

    fix python-sphinx-bootstrap-theme/lilac.py

--- python-sphinx-bootstrap-theme/lilac.py
+++ python-sphinx-bootstrap-theme/lilac.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
+depends = ['python-sphinx-bootstrap-theme']
 pre_build = aur_pre_build
 post_build = aur_post_build
 
