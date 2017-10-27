c076a9d7eadde978c2d4439e49dd8d31296eb1d1
Jingbei Li <i@jingbei.li>
2017-10-27 20:45:05 +0800

    fix intel-parallel-studio-xe/lilac.py

--- intel-parallel-studio-xe/lilac.py
+++ intel-parallel-studio-xe/lilac.py
@@ -3,7 +3,16 @@
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
+
+def pre_build():
+    aur_pre_build()
+    for line in edit_file('PKGBUILD'):
+        if 'packager' in line:
+            continue
+        if 'PKGEXT' in line:
+            continue
+        print(line)
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
