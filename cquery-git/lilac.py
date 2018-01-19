ffe93847696b91216010a8dbf5ea6287404a9386
Jingbei Li <i@jingbei.li>
2018-01-19 16:01:54 +0800

    fix cquery-git/lilac.py

--- cquery-git/lilac.py
+++ cquery-git/lilac.py
@@ -5,8 +5,8 @@ build_prefix = 'arch4edu-x86_64'
 depends=['libtinfo5']
 
 def pre_build():
-    aur_pre_build()
     run_cmd('sh update-submodules.sh cquery'.split(' '))
+    aur_pre_build(do_vcs_update=False)
 
 post_build = aur_post_build
 
