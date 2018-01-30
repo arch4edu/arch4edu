90001083d130d1763fe0619e29895015d9049c77
Jingbei Li <i@jingbei.li>
2018-01-30 13:14:15 +0800

    fix cquery-git/lilac.py

--- cquery-git/lilac.py
+++ cquery-git/lilac.py
@@ -6,10 +6,10 @@ build_prefix = 'extra-x86_64'
 makechrootpkg_args = ['-D', os.path.realpath('cquery')]
 
 def pre_build():
-    run_cmd('sh update-submodules.sh cquery'.split(' '))
     git_rm_files(['update-submodules.sh'])
     aur_pre_build(do_vcs_update=False)
     git_add_files('update-submodules.sh')
+    run_cmd('sh update-submodules.sh cquery'.split(' '))
 
 def post_build():
     aur_post_build()
