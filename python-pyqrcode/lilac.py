5af3e6318d9057b5e6445c12c50826c9776f54ab
Jingbei Li <i@jingbei.li>
2018-02-13 23:08:37 +0800

    remove *.sig for python-pyqrcode

--- python-pyqrcode/lilac.py
+++ python-pyqrcode/lilac.py
@@ -3,7 +3,10 @@ from lilaclib import *
 
 build_prefix = 'extra-x86_64'
 pre_build = aur_pre_build
-post_build = aur_post_build
+
+def post_build():
+    run_cmd('rm LICENSE.sig *.tar.gz.sig'.split(' '))
+    aur_post_build()
 
 if __name__ == '__main__':
   single_main(build_prefix)
