465c571be234b007bc884aac19ac781ecc2e71a4
Jingbei Li <i@jingbei.li>
2018-02-14 13:53:28 +0800

    fix python-pyqrcode/lilac.py

--- python-pyqrcode/lilac.py
+++ python-pyqrcode/lilac.py
@@ -5,8 +5,8 @@ build_prefix = 'extra-x86_64'
 pre_build = aur_pre_build
 
 def post_build():
-    run_cmd('rm LICENSE.sig *.tar.gz.sig'.split(' '))
-    aur_post_build()
+    git_add_files(['PKGBUILD','LICENSE'])
+    git_commit()
 
 if __name__ == '__main__':
   single_main(build_prefix)
