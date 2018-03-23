f8e0624d0caec2fad0ee3737bc7dd103057c7981
Jingbei Li <i@jingbei.li>
2018-03-23 22:25:33 +0800

    fix fetch-sources-from-archlinuxcn.sh

--- vim-youcompleteme-git/fetch-sources-from-archlinuxcn.sh
+++ vim-youcompleteme-git/fetch-sources-from-archlinuxcn.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-sources=$(wget -q -O - https://github.com/archlinuxcn/repo/tree/master/$1 | sed -n '/<td class="content">/{n;s/^.*<a href="[^"]*\/\([^"\/]*\)".*$/\1/;p}')
-git add $0
+git rm --cached -f $(git ls-files --exclude `basename $0`)
+git add $0 lilac.py
+sources=$(wget -q -O - https://github.com/archlinuxcn/repo/tree/master/$1 | sed -n '/<td class="content">/{n;s/.*<a [^>]*>\([^<]*\)<.*/\1/;p}')
 for i in $sources
 do
 	[ $i == 'lilac.py' ] && continue
