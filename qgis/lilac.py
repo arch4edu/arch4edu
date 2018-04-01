e3f7af8df0af0d07d85ae25074c9ba0bc6922174
Jingbei Li <i@jingbei.li>
2018-04-01 18:10:23 +0800

    added proxy for qgis

--- qgis/lilac.py
+++ qgis/lilac.py
@@ -3,8 +3,16 @@ from lilaclib import *
 
 build_prefix = 'arch4edu-x86_64'
 depends = ['qwtpolar', 'qextserialport']
-pre_build = aur_pre_build
-post_build = aur_post_build
+
+def pre_build():
+    aur_pre_build()
+    os.environ['http_proxy']='127.0.0.1:8123'
+    os.environ['https_proxy']='127.0.0.1:8123'
+
+def post_build():
+    aur_post_build()
+    del os.environ['http_proxy']
+    del os.environ['https_proxy']
 
 if __name__ == '__main__':
   single_main(build_prefix)
