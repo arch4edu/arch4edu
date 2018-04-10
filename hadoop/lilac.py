0eaf5a062114477b3ac0ffa3b1401e9c44661ee9
Jingbei Li <i@jingbei.li>
2018-04-11 03:29:17 +0800

    change mirror for hadoop

--- hadoop/lilac.py
+++ hadoop/lilac.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 from lilaclib import *
 
 build_prefix = 'arch4edu-x86_64'
@@ -8,10 +7,14 @@ def pre_build():
   aur_pre_build()
 
   for line in edit_file('PKGBUILD'):
-    if not 'PKGEXT=' in line:
+    if 'PKGEXT=' in line:
+        continue
+    if 'http://www-us.apache.org/dist/|' in line:
+        print(line.replace('http://www-us.apache.org/dist/|', 'http://mirrors.tuna.tsinghua.edu.cn/apache/'))
+    else:
         print(line)
 
 post_build = aur_post_build
 
 if __name__ == '__main__':
-  single_main()
+  single_main(build_prefix)
