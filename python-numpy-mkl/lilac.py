748f9b7097e70088354ed6d9021b62b85d2f9139
Jingbei Li <i@jingbei.li>
2018-05-02 22:53:59 +0800

    addpkg:	python-numpy-mkl #14

---
+++ python-numpy-mkl/lilac.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from lilaclib import *
+
+build_prefix = 'arch4edu-x86_64'
+depends = ['intel-parallel-studio-xe']
+makechrootpkg_args = ['-D', '/opt/intel/licenses']
+
+def pre_build():
+    aur_pre_build()
+
+    for line in edit_file('PKGBUILD'):
+        if 'build() {' in line:
+            print(line)
+            print('\texport __INTEL_PRE_CFLAGS="$__INTEL_PRE_CFLAGS -D__PURE_INTEL_C99_HEADERS__ -D_Float32=float -D_Float64=double -D_Float128=\\"long double\\" -D_Float32x=_Float64 -D_Float64x=_Float128"')
+        else:
+            print(line)
+
+post_build = aur_post_build
+
+if __name__ == '__main__':
+  single_main(build_prefix)
