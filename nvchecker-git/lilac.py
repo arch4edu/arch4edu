6ad96b84170db694281c75109042c3af745c4963
Jingbei Li <i@jingbei.li>
2018-05-09 03:01:25 +0800

    fix build_prefix for nvchecker-git

--- nvchecker-git/lilac.py
+++ nvchecker-git/lilac.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
+build_prefix = 'arch4edu-x86_64'
 depends = ['python-structlog']
 pre_build = aur_pre_build
 post_build = aur_post_build
