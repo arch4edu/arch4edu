b32ee92796ab6f3a1be19ca40a5e4603eb4a2623
Jingbei Li <i@jingbei.li>
2018-05-21 02:36:25 +0800

    added gcc6 to cntk/lilac.py

--- cntk/lilac.py
+++ cntk/lilac.py
@@ -2,7 +2,7 @@
 from lilaclib import *
 
 build_prefix = 'arch4edu-x86_64'
-depends=['cub', 'openblas-lapack']
+depends=['cub', 'gcc6', 'openblas-lapack']
 pre_build = aur_pre_build
 post_build = aur_post_build
 
