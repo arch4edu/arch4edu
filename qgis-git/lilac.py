b81bbe3ea784296ece04def24322081a6d4e45a8
Winston Wu <winstonwu91@gmail.com>
2018-06-23 13:43:06 +0000

    Modify lilac.py for qgis-git

--- qgis-git/lilac.py
+++ qgis-git/lilac.py
@@ -4,8 +4,11 @@ from lilaclib import *
 
 build_prefix = 'arch4edu-x86_64'
 depends=['grass']
-pre_build = aur_pre_build
-post_build = aur_post_build
+pre_build = vcs_update
 
+def post_build():
+  git_add_files('PKGBUILD')
+  git_commit()
+  
 if __name__ == '__main__':
-    single_main(build_prefix)
+  single_main()
