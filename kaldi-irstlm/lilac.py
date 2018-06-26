ee193926d62ad386cddc6394ebcb98cb6a5753d5
Jingbei Li <i@jingbei.li>
2018-06-26 17:54:06 +0800

    fix kaldi-irstlm/lilac.py

--- kaldi-irstlm/lilac.py
+++ kaldi-irstlm/lilac.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
-
 from lilaclib import *
 
-build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
+build_prefix = 'arch4edu-x86_64'
+depends = ['kaldi']
+
+def pre_build():
+    aur_pre_build()
+    run_cmd(['rm', '-f', 'master.zip'])
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
