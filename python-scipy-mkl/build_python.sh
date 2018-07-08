d5bf821403f44e55e408198e2c25b51b0288a151
calil <calil@jingbei.li>
2018-07-08 13:18:08 +0800

    auto update for package python-scipy-mkl

--- python-scipy-mkl/build_python.sh
+++ python-scipy-mkl/build_python.sh
@@ -3,6 +3,7 @@
 # the intel C compiler lack support for float128 while glibc expects this.
 # The set environment variable, however, precludes compiling C++ code
 # So, if the build process fails, it is re-run without the environment variable und continues until failing again etc.
+set +e
 build="$1 setup.py config --compiler=intelem --fcompiler=intelem build_clib --compiler=intelem --fcompiler=intelem build_ext --compiler=intelem --fcompiler=intelem"
 
 method1() {
@@ -15,9 +16,8 @@ method2() {
 	__INTEL_PRE_CFLAGS="$__INTEL_PRE_CFLAGS -D_Float32=float -D_Float64=double -D_Float128=\"long double\" -D_Float32x=_Float64 -D_Float64x=_Float128" $build
 }
 
-# It will end at 'Retry: 5 Method: 2' for scipy 1.1.0
-for retry in `seq 10`
-do
+# It will end at 'Retry: 5 Method: 2' for scipy 1.1.0 and intel-parallel-studio-xe 2018.3.222
+for retry in $(seq 10); do
 	method1 && break
 	method2 && break
 done
