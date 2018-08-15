76484627018b56645de0b1270705eff10cfcf676
Jingbei Li <i@jingbei.li>
2018-08-15 17:39:27 +0800

    fix python 3.7 for pocketsphinx and sphinxbase

--- pocketsphinx/lilac.py
+++ pocketsphinx/lilac.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
-#
-# This file is the most simple lilac.py file,
-# and it suits for most packages in AUR.
-#
-
 from lilaclib import *
 
 build_prefix = 'arch4edu-x86_64'
 depends=['sphinxbase']
-pre_build = aur_pre_build
+
+def pre_build():
+    aur_pre_build()
+
+    for line in edit_file('PKGBUILD'):
+        if 'python3.6' in line:
+            print(line.replace('python3.6', 'python3.7'))
+        else:
+            print(line)
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
-  single_main()
+  single_main(build_prefix)
