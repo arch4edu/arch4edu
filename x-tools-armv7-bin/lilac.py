ec680a3acd68c3f67fc3139350f0c8c6dec98795
Jingbei Li <i@jingbei.li>
2018-09-05 15:47:31 +0800

    fix lilac.py for x-tools-armv{6,7,8}-bin

--- x-tools-armv7-bin/lilac.py
+++ x-tools-armv7-bin/lilac.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
-
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
+
+def pre_build():
+    aur_pre_build()
+    run_cmd(['rm', '-f', 'x-tools7h.tar.xz'])
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
