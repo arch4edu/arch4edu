6d13f56c0f5311fd6c3483ac1cfa156a283f9092
Jingbei Li <i@jingbei.li>
2018-09-30 17:10:21 +0800

    fix plantuml/lilac.py

--- plantuml/lilac.py
+++ plantuml/lilac.py
@@ -3,21 +3,11 @@
 from lilaclib import *
 
 build_prefix = 'extra-x86_64'
-
-def pre_build():
-    aur_pre_build()
-
-    for line in edit_file('PKGBUILD'):
-        if 'makedepends' in line:
-            print(line.replace('java-environment=8', 'java-environment=7'))
-        elif 'sed' in line and '1.8' in line:
-            print(line.replace('1.8', '1.7'))
-        else:
-            print(line)
+pre_build = aur_pre_build
 
 def post_build():
     run_cmd('git add -f plantuml.run'.split(' '))
-    aur_post_build
+    aur_post_build()
 
 if __name__ == '__main__':
   single_main(build_prefix)
