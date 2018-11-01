8facf72e1cc955c9e730bb7942c08d50e66ced14
Jingbei Li <i@jingbei.li>
2018-11-01 14:43:49 +0800

    updated eclim/lilac.py

--- eclim/lilac.py
+++ eclim/lilac.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
 from lilaclib import *
 
+update_on = [{'aur': None}]
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
+
+def pre_build():
+    aur_pre_build()
+
+    for line in edit_file('PKGBUILD'):
+        if line.startswith('depends=('):
+            line.replace("'eclipse'", '')
+        elif line.startswith('makedepends=('):
+            line.replace(')', '"eclipse-jee")')
+        elif line.startswith('package()'):
+            print(line)
+            print('  depends+=("eclipse")')
+        else:
+            print(line)
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
-  single_main(build_prefix)
+    single_main(build_prefix)
