412764547d14e013483c666915ea3aca42da662d
Jingbei Li <i@jingbei.li>
2018-11-05 16:38:07 +0800

    updated python{,2}-llvmlite/lilac.py

--- python-llvmlite/lilac.py
+++ python-llvmlite/lilac.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
 from lilaclib import *
 
+update_on = [{'aur': None}]
 build_prefix = 'extra-x86_64'
-depends = ['llvm50']
-pre_build = aur_pre_build
+
+def pre_build():
+    aur_pre_build()
+
+    for line in edit_file('PKGBUILD'):
+        if line.startswith('depends=('):
+            print(line.replace("'llvm>=6.0.0' 'llvm<6.1.0'", '"llvm6"').replace('llvm-libs', 'llvm6-libs'))
+        elif line.startswith('makedepends=('):
+            print(line.replace(" 'llvm'", ''))
+        else:
+            print(line)
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
-  single_main(build_prefix)
+    single_main(build_prefix)
