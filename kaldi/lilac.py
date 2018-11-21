d087ad13c280f1285f0ef85d4a448b645ffcd83c
Jingbei Li <i@jingbei.li>
2018-11-21 19:06:58 +0800

    updated kaldi/lilac.py

--- kaldi/lilac.py
+++ kaldi/lilac.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 from lilaclib import *
 
+update_on = [{'aur': None}, {'github': 'kaldi-asr/kaldi'}]
 build_prefix = 'arch4edu-x86_64'
-depends=['gcc6', 'kaldi-openfst', 'openblas-lapack']
+depends=['cub', 'kaldi-openfst', 'openblas-lapack']
 
 def pre_build():
     aur_pre_build()
@@ -16,4 +17,4 @@ def pre_build():
 post_build = aur_post_build
 
 if __name__ == '__main__':
-  single_main(build_prefix)
+    single_main(build_prefix)
