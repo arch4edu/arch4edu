c07e329e898bad9a24b200b14c34478c889796ca
Jingbei Li <i@jingbei.li>
2019-01-10 15:33:36 +0800

    rmpkg:	intel-tbb-gcc6 not needed by mxnet anymore

--- mxnet/lilac.py
+++ mxnet/lilac.py
@@ -3,7 +3,7 @@ from lilaclib import *
 
 update_on = [{'aur': None}]
 build_prefix = 'arch4edu-x86_64'
-depends = ['gcc6', 'intel-tbb-gcc6', 'openblas-lapack']
+depends = ['openblas-lapack']
 pre_build = aur_pre_build
 post_build = aur_post_build
 
