6e0f788d2392b3914b39dd6fe8e743502c01c8a5
Jingbei Li <i@jingbei.li>
2019-01-10 15:46:30 +0800

    trigger rebuild for  python-scipy-mkl dropped python2-scipy-mkl

--- python-scipy-mkl/lilac.py
+++ python-scipy-mkl/lilac.py
@@ -5,7 +5,20 @@ update_on = [{'aur': None}]
 build_prefix = 'arch4edu-x86_64'
 depends = [('intel-parallel-studio-xe', 'intel-compiler-base'), ('intel-parallel-studio-xe', 'intel-fortran-compiler'), ('intel-parallel-studio-xe', 'intel-mkl')]
 makechrootpkg_args = ['-D', '/opt/intel/licenses']
-pre_build = aur_pre_build
+
+def pre_build():
+    aur_pre_build()
+
+    for line in edit_file('PKGBUILD'):
+        if 'replaces' in line:
+            continue
+        elif 'sh build_python.sh python2'
+            print('\t\t;')
+        elif line.startswith('pkgname='):
+            print("pkgname='python-scipy-mkl'")
+        else:
+            print(line)
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
