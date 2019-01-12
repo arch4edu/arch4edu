3aaca961b20a59c03619c9692da06d8b54fe16bc
Jingbei Li <i@jingbei.li>
2019-01-12 15:56:12 +0800

    rm update-submodules.sh for cquery-git

--- cquery-git/lilac.py
+++ cquery-git/lilac.py
@@ -4,14 +4,7 @@ import os
 
 update_on = [{'aur': None}, {'github': 'jacobdufault/cquery'}]
 build_prefix = 'extra-x86_64'
-makechrootpkg_args = ['-D', os.path.realpath('cquery')]
-
-def pre_build():
-    git_rm_files(['update-submodules.sh'])
-    aur_pre_build(do_vcs_update=False)
-    git_add_files('update-submodules.sh')
-    run_cmd('sh update-submodules.sh cquery'.split(' '))
-
+pre_build = aur_pre_build
 post_build = aur_post_build
 
 if __name__ == '__main__':
