ab427d1f3982e104f7f3ae73a7b4d59402d00d2e
Jingbei Li <i@jingbei.li>
2019-01-14 17:28:53 +0800

    fixed vim-gitgutter-git/lilac.py

--- vim-gitgutter-git/lilac.py
+++ vim-gitgutter-git/lilac.py
@@ -3,7 +3,14 @@ from lilaclib import *
 
 update_on = [{'aur': None}, {'github': 'airblade/vim-gitgutter'}]
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
+
+def pre_build():
+    aur_pre_build()
+
+    for line in edit_file('PKGBUILD'):
+        if not line.startswith('groups=('):
+            print(line)
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
