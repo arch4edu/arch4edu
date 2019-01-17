321dd3956ebbb6b43793177abb99ef3a27252b7e
Jingbei Li <i@jingbei.li>
2019-01-17 12:36:35 +0800

    fixed android-studio/lilac.py

--- android-studio/lilac.py
+++ android-studio/lilac.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 from lilaclib import *
+import os
 
 update_on = [{'aur': None}]
 build_prefix = 'multilib'
-pre_build = aur_pre_build
-post_build = aur_post_build
+
+def pre_build():
+    aur_pre_build()
+    os.environ['http_proxy'] = '127.0.0.1:8123'
+    os.environ['https_proxy'] = '127.0.0.1:8123'
+
+def post_build():
+    aur_post_build()
+    del os.environ['http_proxy']
+    del os.environ['https_proxy']
 
 if __name__ == '__main__':
     single_main(build_prefix)
