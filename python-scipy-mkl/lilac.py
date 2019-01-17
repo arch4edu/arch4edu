4b272206e54464577e74cf41aef781cfa00a2c9c
Jingbei Li <i@jingbei.li>
2019-01-17 12:36:46 +0800

    fixed python-scipy-mkl/lilac.py

--- python-scipy-mkl/lilac.py
+++ python-scipy-mkl/lilac.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from lilaclib import *
 
-update_on = [{'aur': None}]
+update_on = [{'aur': None}, {'archpkg': 'python'}]
 build_prefix = 'arch4edu-x86_64'
 depends = [('intel-parallel-studio-xe', 'intel-compiler-base'), ('intel-parallel-studio-xe', 'intel-fortran-compiler'), ('intel-parallel-studio-xe', 'intel-mkl')]
 makechrootpkg_args = ['-D', '/opt/intel/licenses']
