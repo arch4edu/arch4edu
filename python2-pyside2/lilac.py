2f4c70036f09f7c152f1b5e865a469c95912c639
Jingbei Li <i@jingbei.li>
2019-01-21 16:18:33 +0800

    fixed python2-pyside2/lilac.py

--- python2-pyside2/lilac.py
+++ python2-pyside2/lilac.py
@@ -4,7 +4,14 @@ from lilaclib import *
 update_on = [{'aur': None}]
 depends = ['python2-shiboken2']
 build_prefix = 'arch4edu-x86_64'
-pre_build = aur_pre_build
+
+def pre_build():
+    aur_pre_build()
+
+    for line in edit_file('PKGBUILD'):
+        if not line.startswith('groups=('):
+            print(line)
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
