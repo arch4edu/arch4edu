37bcb6b8c885fa1b8cdcc59ec7f2fc55ae57f797
Jingbei Li <i@jingbei.li>
2019-01-28 17:30:56 +0800

    addpkg:	devtools-alarm

---
+++ devtools-alarm/lilac.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+from lilaclib import *
+
+update_on = [{'archkpkg': 'devtools'}, {'github': 'archlinuxarm/PKGBUILDs', 'path': 'alarm/devtools-alarm'}]
+build_prefix = 'extra-x86_64'
+
+def pre_build():
+    run_cmd(['sh', 'download-files-from-github.sh', 'archlinuxarm/PKGBUILDs', 'alarm/devtools-alarm'])
+    vcs_update()
+
+def post_build():
+    git_add_files('PKGBUILD')
+    git_commit()
+
+if __name__ == '__main__':
+    single_main(build_prefix)
