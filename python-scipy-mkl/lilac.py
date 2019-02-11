9671511f581766049865c62c79f35fcfb0d8aab5
Jingbei Li <i@jingbei.li>
2019-02-11 18:15:09 +0800

    build python-scipy-mkl with python-numpy-mkl

--- python-scipy-mkl/lilac.py
+++ python-scipy-mkl/lilac.py
@@ -5,7 +5,16 @@ update_on = [{'aur': None}, {'archpkg': 'python'}]
 build_prefix = 'arch4edu-x86_64'
 depends = [('intel-parallel-studio-xe', 'intel-compiler-base'), ('intel-parallel-studio-xe', 'intel-fortran-compiler'), ('intel-parallel-studio-xe', 'intel-mkl')]
 makechrootpkg_args = ['-D', '/opt/intel/licenses']
-pre_build = aur_pre_build
+
+def pre_build():
+    aur_pre_build()
+
+    for line in edit_file('PKGBUILD'):
+        if line.startswith('makedepends=('):
+            print(line.replace('numpy', 'numpy-mkl'))
+        else:
+            print(line)
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
