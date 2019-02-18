9ca5852799319cfe877a1b0b0e86664fc334462d
Jingbei Li <i@jingbei.li>
2019-02-18 18:03:55 +0800

    fixed kiwiirc/lilac.py

--- kiwiirc/lilac.py
+++ kiwiirc/lilac.py
@@ -3,7 +3,10 @@ from lilaclib import *
 
 update_on = [{'aur': None}, {'github': 'kiwiirc/kiwiirc'}]
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
+
+def pre_build():
+    aur_pre_build(do_vcs_update=True)
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
