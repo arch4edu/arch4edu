834b21ccd682cd30ff53c7ff2eff92e91c6a8a93
Jingbei Li <i@jingbei.li>
2019-02-20 12:45:16 +0800

    rmpkg:	pythia, root, root-extra moved to [community]

--- pythia/pythia.sh
+++
@@ -1,3 +0,0 @@
-# /etc/profile.d/pythia.sh
-export PYTHIA8=/usr
-export PYTHIA8DATA=$PYTHIA8/share/pythia/xmldoc
--- root-extra/root.sh
+++
@@ -1,9 +0,0 @@
-if [ $PYTHONPATH ]; then
-    if [[ ":$PYTHONPATH:" == *":/usr/lib/root:"* ]]; then
-        return 0;
-    else
-        export PYTHONPATH=$PYTHONPATH:/usr/lib/root;
-    fi
-else
-    export PYTHONPATH=/usr/lib/root;
-fi
