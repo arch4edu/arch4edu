fb503f75e7b4ef8fedd2ee0275022b21626343ea
Jingbei Li <i@jingbei.li>
2019-02-21 16:29:15 +0800

    increased time limit for mongodb

--- mongodb/lilac.py
+++ mongodb/lilac.py
@@ -5,6 +5,7 @@ import os
 update_on = [{'aur': None}]
 build_prefix = 'arch4edu-x86_64'
 build_args = ['-r', os.path.expanduser('~/.lilac/archbuild')]
+time_limit_hours = 4
 pre_build = aur_pre_build
 post_build = aur_post_build
 
