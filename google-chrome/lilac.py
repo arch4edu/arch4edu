2fde689350477e564a1117479a15d1f14e725f1e
Jingbei Li <i@jingbei.li>
2019-02-26 14:32:24 +0800

    fixed google-chrome/lilac.py

--- google-chrome/lilac.py
+++ google-chrome/lilac.py
@@ -6,9 +6,9 @@ update_on = [{'aur': None}]
 build_prefix = 'extra-x86_64'
 
 def pre_build():
-    aur_pre_build()
     os.environ['http_proxy'] = '127.0.0.1:8123'
     os.environ['https_proxy'] = '127.0.0.1:8123'
+    aur_pre_build()
 
 def post_build():
     aur_post_build()
