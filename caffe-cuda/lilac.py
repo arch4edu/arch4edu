2d46b13c6772280e2500ee8173b611036ebd939e
Jingbei Li <i@jingbei.li>
2019-03-05 14:14:23 +0800

    fixed caffe{,-cuda}/lilac.py

--- caffe-cuda/lilac.py
+++ caffe-cuda/lilac.py
@@ -3,8 +3,12 @@ from lilaclib import *
 
 update_on = [{'aur': None}]
 build_prefix = 'arch4edu-x86_64'
-depends = ['openblas-lapack', 'python-scikit-image','python-pydotplus', 'python-leveldb']
-pre_build = aur_pre_build
+depends = ['python-scikit-image','python-pydotplus', 'python-leveldb']
+
+def pre_build():
+    aur_pre_build()
+    add_makedepends(['texlive-latexextra'])
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
--- caffe/lilac.py
+++ caffe/lilac.py
@@ -4,7 +4,11 @@ from lilaclib import *
 update_on = [{'aur': None}]
 build_prefix = 'arch4edu-x86_64'
 depends = ['python-scikit-image','python-pydotplus', 'python-leveldb']
-pre_build = aur_pre_build
+
+def pre_build():
+    aur_pre_build()
+    add_makedepends(['texlive-latexextra'])
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
