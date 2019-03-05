978af9ac321d2727608f245112ab98d2d5b76d91
Jingbei Li <i@jingbei.li>
2019-03-05 15:53:06 +0800

    fixed caffe-cuda/lilac.py

--- caffe-cuda/lilac.py
+++ caffe-cuda/lilac.py
@@ -3,7 +3,7 @@ from lilaclib import *
 
 update_on = [{'aur': None}]
 build_prefix = 'arch4edu-x86_64'
-depends = ['python-scikit-image','python-pydotplus', 'python-leveldb']
+depends = ['openblas-lapack', 'python-scikit-image','python-pydotplus', 'python-leveldb']
 
 def pre_build():
     aur_pre_build()
