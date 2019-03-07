21b9b9f1b0c318e83b82e1e192496ada2f7b73f3
Jingbei Li <i@jingbei.li>
2019-03-07 15:15:26 +0800

    set proxy for gmsh

--- gmsh/lilac.py
+++ gmsh/lilac.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from lilaclib import *
+import os
 
 update_on = [{'aur': None}]
 build_prefix = 'arch4edu-x86_64'
@@ -8,8 +9,13 @@ depends = ['med-salome']
 def pre_build():
     aur_pre_build()
     add_makedepends(['gcc-fortran'])
+    os.environ['http_proxy'] = '127.0.0.1:8123'
+    os.environ['https_proxy'] = '127.0.0.1:8123'
 
-post_build = aur_post_build
+def post_build():
+    aur_post_build()
+    del os.environ['http_proxy']
+    del os.environ['https_proxy']
 
 if __name__ == '__main__':
     single_main(build_prefix)
