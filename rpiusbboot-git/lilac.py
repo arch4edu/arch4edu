06f864571bdc239dbc4aada0faa638f90331cc8c
Jingbei Li <i@jingbei.li>
2019-03-13 15:11:24 +0800

    fixed upstream for rpiusbboot-git

--- rpiusbboot-git/lilac.py
+++ rpiusbboot-git/lilac.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from lilaclib import *
 
-update_on = [{'aur': None}]
+update_on = [{'aur': None}, {'github': 'raspberrypi/usbboot'}]
 build_prefix = ['extra-x86_64', 'extra-armv6h', 'extra-armv7h', 'extra-aarch64']
 
 def pre_build():
