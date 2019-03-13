9258e3313baee55d9159a17faab1ccdd0c0c53f3
Jingbei Li <i@jingbei.li>
2019-03-13 14:39:34 +0800

    fixed rpiusbboot-git/lilac.py

--- rpiusbboot-git/lilac.py
+++ rpiusbboot-git/lilac.py
@@ -6,6 +6,7 @@ build_prefix = ['extra-x86_64', 'extra-armv6h', 'extra-armv7h', 'extra-aarch64']
 
 def pre_build():
     aur_pre_build()
+    add_arch(['armv6h', 'armv7h', 'aarch64'])
     add_makedepends(['git'])
 
 post_build = aur_post_build
