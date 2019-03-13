e34b6363b531aec4d661974f145b8f59daf278fb
Jingbei Li <i@jingbei.li>
2019-03-13 13:52:48 +0800

    fixed pgp for archlinuxarm-keyring

--- archlinuxarm-keyring/lilac.py
+++ archlinuxarm-keyring/lilac.py
@@ -3,10 +3,10 @@ from lilaclib import *
 
 update_on = [{'github': 'archlinuxarm/PKGBUILDs', 'path': 'core/archlinuxarm-keyring'}]
 build_prefix = 'extra-x86_64'
-makepkg_args = ['--skippgpcheck']
 
 def pre_build():
     run_cmd(['sh', 'download-files-from-github.sh', 'archlinuxarm/PKGBUILDs', 'core/archlinuxarm-keyring'])
+    add_into_array('validpgpkeys', ['77193F152BDBE6A6', '68B3537F39A313B3E574D06777193F152BDBE6A6'])
 
 def post_build():
     git_add_files('PKGBUILD')
