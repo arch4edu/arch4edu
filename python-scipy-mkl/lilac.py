fb4d4ff0667b9ca8c7c0bb67c2395468c896bb32
Jingbei Li <i@jingbei.li>
2019-03-31 12:18:54 +0800

    fixed python-scipy-mkl/lilac.py

--- python-scipy-mkl/lilac.py
+++ python-scipy-mkl/lilac.py
@@ -3,7 +3,7 @@ from lilaclib import *
 
 update_on = [{'aur': None}, {'archpkg': 'python'}]
 build_prefix = 'extra-x86_64'
-depends = [('intel-parallel-studio-xe', 'intel-compiler-base'), ('intel-parallel-studio-xe', 'intel-fortran-compiler'), ('intel-parallel-studio-xe', 'intel-mkl')]
+depends = [('intel-parallel-studio-xe', 'intel-compiler-base'), ('intel-parallel-studio-xe', 'intel-fortran-compiler'), ('intel-parallel-studio-xe', 'intel-mkl'), 'python-numpy-mkl', ('python-numpy-mkl', 'python2-numpy-mkl')]
 makechrootpkg_args = ['-D', '/opt/intel/licenses']
 
 def pre_build():
