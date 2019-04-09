a46bd262a617219bdc4f26dccd2462f720c6ad02
Jingbei Li <i@jingbei.li>
2019-04-09 15:17:05 +0800

    increased time limit for yay

--- yay/lilac.py
+++ yay/lilac.py
@@ -3,6 +3,7 @@ from lilaclib import *
 
 update_on = [{'aur': None}]
 build_prefix = ['extra-x86_64', 'extra-armv6h', 'extra-armv7h', 'extra-aarch64']
+time_limit_hours = 3
 pre_build = aur_pre_build
 post_build = aur_post_build
 
