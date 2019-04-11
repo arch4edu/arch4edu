4c492bf8826470debb9f957853c0172b98ac6703
Jingbei Li <i@jingbei.li>
2019-04-11 17:05:04 +0800

    added python-imageio for x86_64

--- python-imageio/lilac.py
+++ python-imageio/lilac.py
@@ -3,7 +3,6 @@ from lilaclib import *
 
 update_on = [{'aur': None}]
 build_prefix = 'extra-x86_64'
-repo_depends = ['python-imageio', 'python-pywavelets']
 pre_build = aur_pre_build
 post_build = aur_post_build
 
