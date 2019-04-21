3a57959a6687c9b48f72faec6089ebcd4822ff29
Jingbei Li <i@jingbei.li>
2019-04-21 15:26:34 +0800

    fixed gmsh/lilac.py

--- gmsh/lilac.py
+++ gmsh/lilac.py
@@ -1,21 +1,12 @@
 #!/usr/bin/env python3
 from lilaclib import *
-import os
 
+maintainers = [{'github': 'petronny'}]
 update_on = [{'aur': None}]
-build_prefix = 'extra-x86_64'
+build_prefix = ['extra-x86_64']
 repo_depends = ['med-salome']
-
-def pre_build():
-    aur_pre_build()
-    add_makedepends(['gcc-fortran'])
-    os.environ['http_proxy'] = '127.0.0.1:8123'
-    os.environ['https_proxy'] = '127.0.0.1:8123'
-
-def post_build():
-    aur_post_build()
-    del os.environ['http_proxy']
-    del os.environ['https_proxy']
+pre_build = aur_pre_build
+post_build = aur_post_build
 
 if __name__ == '__main__':
     single_main(build_prefix)
