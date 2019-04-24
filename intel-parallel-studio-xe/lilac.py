77091ef6c9349537ca444dd7bd62a62020274449
Jingbei Li <i@jingbei.li>
2019-04-24 10:56:30 +0800

    fixed intel-parallel-studio-xe/lilac.py

--- intel-parallel-studio-xe/lilac.py
+++ intel-parallel-studio-xe/lilac.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python3
 from lilaclib import *
 
+maintainers = [{'github': 'petronny'}]
 update_on = [{'aur': 'intel-mkl'}]
 build_prefix = 'extra-x86_64'
-
-def pre_build():
-    aur_pre_build()
-
-    for line in edit_file('PKGBUILD'):
-        if 'packager' in line:
-            continue
-        if 'PKGEXT' in line:
-            continue
-        print(line)
-
+pre_build = aur_pre_build
 post_build = aur_post_build
 
 if __name__ == '__main__':
