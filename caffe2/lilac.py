cbccbfb40308ec23fcf65c58a22ac99f4dc3012b
Jingbei Li <i@jingbei.li>
2019-05-05 18:27:00 +0800

    increased time limit for caffe2

--- caffe2/lilac.py
+++ caffe2/lilac.py
@@ -5,6 +5,7 @@ maintainers = [{'github': 'petronny'}]
 update_on = [{'aur': None}]
 build_prefix = 'extra-x86_64'
 repo_depends = ['confu-git', 'python-peachpy-git', 'python-scikit-image', 'python-lmdb', 'python-leveldb', 'python-glog', 'python-nvd3', 'python-slugify', 'rdma-core']
+time_limit_hours = 3
 pre_build = aur_pre_build
 post_build = aur_post_build
 
