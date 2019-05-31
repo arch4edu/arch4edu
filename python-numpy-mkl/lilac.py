2a2b0571f7a501b9574e9b8d95e13158b542c884
Jingbei Li <i@jingbei.li>
2019-05-31 17:20:27 +0800

    fixed python-numpy-mkl/lilac.py

--- python-numpy-mkl/lilac.py
+++ python-numpy-mkl/lilac.py
@@ -6,19 +6,7 @@ update_on = [{'aur': None}]
 build_prefix = 'extra-x86_64'
 repo_depends = [('intel-parallel-studio-xe', 'intel-common-libs'), ('intel-parallel-studio-xe', 'intel-compiler-base'), ('intel-parallel-studio-xe', 'intel-fortran-compiler'), ('intel-parallel-studio-xe', 'intel-mkl'), ('intel-parallel-studio-xe', 'intel-openmp')]
 makechrootpkg_args = ['-D', '/opt/intel/licenses']
-
-def pre_build():
-    aur_pre_build()
-
-    for line in edit_file('PKGBUILD'):
-        if line.endswith('${srcdir}/fix_compiler.patch'):
-            continue
-        elif line.startswith('\tpatch'):
-            print(line)
-            print('\tpatch ${srcdir}/numpy-${pkgver}/numpy/distutils/ccompiler.py < ${srcdir}/fix_compiler.patch')
-        else:
-            print(line)
-
+pre_build = aur_pre_build
 post_build = aur_post_build
 
 if __name__ == '__main__':
