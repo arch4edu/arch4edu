f5036800ca0c59367eb6370155271d0ed743a18e
Jingbei Li <i@jingbei.li>
2019-06-05 04:21:38 +0800

    fixed packages for arch4edu/docker-image

--- python-mkl-random/lilac.py
+++ python-mkl-random/lilac.py
@@ -4,7 +4,7 @@ from lilaclib import *
 maintainers = [{'github': 'petronny'}]
 update_on = [{'aur': None}]
 build_prefix = 'extra-x86_64'
-repo_depends = [('intel-parallel-studio-xe', 'intel-common-libs'), ('intel-parallel-studio-xe', 'intel-compiler-base'), ('intel-parallel-studio-xe', 'intel-mkl')]
+repo_depends = [('intel-parallel-studio-xe', 'intel-common-libs'), ('intel-parallel-studio-xe', 'intel-compiler-base'), ('intel-parallel-studio-xe', 'intel-mkl'), ('intel-parallel-studio-xe', 'intel-openmp')]
 pre_build = aur_pre_build
 post_build = aur_post_build
 
