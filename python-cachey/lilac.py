6d1b0b05e2cc6f2afe56b72f79f0ff695f2aa334
Jingbei Li <i@jingbei.li>
2019-06-20 13:45:06 +0800

    fixed python-cachey/lilac.py

--- python-cachey/lilac.py
+++ python-cachey/lilac.py
@@ -4,7 +4,7 @@ from lilaclib import *
 maintainers = [{'github': 'petronny'}]
 update_on = [{'aur': None}]
 build_prefix = 'extra-x86_64'
-repo_depends = ['python-heapdict']
+repo_depends = ['python-heapdict', ('python-heapdict', 'python2-heapdict')]
 pre_build = aur_pre_build
 post_build = aur_post_build
 
