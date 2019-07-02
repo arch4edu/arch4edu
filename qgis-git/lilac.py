c6a8979c966243727b4a1771a8c9ac203475d9eb
Jingbei Li <i@jingbei.li>
2019-07-02 13:52:54 +0800

    fixed qgis-git/lilac.py

--- qgis-git/lilac.py
+++ qgis-git/lilac.py
@@ -4,12 +4,7 @@ from lilaclib import *
 maintainers = [{'github': 'heavysink', 'email': 'Heavysink <winstonwu91@gmail.com>'}, {'github': 'petronny'}]
 update_on = [{'aur': None}, {'github': 'qgis/QGIS'}]
 build_prefix = 'extra-x86_64'
-repo_depends = ['grass']
-
-def pre_build():
-    aur_pre_build()
-    add_depends(['qt5-xmlpatterns'])
-
+pre_build = aur_pre_build
 post_build = aur_post_build
 
 if __name__ == '__main__':
