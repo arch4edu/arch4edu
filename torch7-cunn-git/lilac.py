04b12c0d535680f22bcf3ae43cc950bb233dd93d
Jingbei Li <i@jingbei.li>
2019-07-03 13:34:00 +0800

    fixed torch7-cunn-git/lilac.py

--- torch7-cunn-git/lilac.py
+++ torch7-cunn-git/lilac.py
@@ -4,7 +4,7 @@ from lilaclib import *
 maintainers = [{'github': 'petronny'}]
 update_on = [{'aur': None}, {'github': 'torch/cunn'}]
 build_prefix = 'extra-x86_64'
-repo_depends = ['torch7-git']
+repo_depends = ['torch7-cutorch-git', 'torch7-git', 'torch7-nn-git']
 pre_build = aur_pre_build
 post_build = aur_post_build
 
