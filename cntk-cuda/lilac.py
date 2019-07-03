cfc1e3a2b585a7a105d41c607d92f7eb099807e9
Jingbei Li <i@jingbei.li>
2019-07-03 13:31:53 +0800

    fixed cntk{,-cuda}/lilac.py

--- cntk-cuda/lilac.py
+++ cntk-cuda/lilac.py
@@ -4,7 +4,7 @@ from lilaclib import *
 maintainers = [{'github': 'petronny'}]
 update_on = [{'aur': None}]
 build_prefix = 'extra-x86_64'
-repo_depends = ['cub', 'gcc7', 'openblas-lapack']
+repo_depends = ['cub', 'gcc7', ('gcc7', 'gcc7-libs'), 'openblas-lapack', 'protobuf-static']
 pre_build = aur_pre_build
 post_build = aur_post_build
 
