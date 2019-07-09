9aeec08816de832ca95e64bacee3e5794d876043
Jingbei Li <i@jingbei.li>
2019-07-09 15:13:12 +0800

    added proxy for vim-youcompleteme-git

--- vim-youcompleteme-git/lilac.py
+++ vim-youcompleteme-git/lilac.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from lilaclib import *
+import os
 
 maintainers = [{'github': 'petronny'}]
 update_on = [{'aur': None}, {'github': 'Valloric/YouCompleteMe'}]
@@ -7,13 +8,19 @@ repo_depends = ['ncurses5-compat-libs']
 build_prefix = 'extra-x86_64'
 
 def pre_build():
+    os.environ['http_proxy'] = '127.0.0.1:8123'
+    os.environ['https_proxy'] = '127.0.0.1:8123'
+
     aur_pre_build()
 
     for line in edit_file('PKGBUILD'):
         if not line.startswith('groups=('):
             print(line)
 
-post_build = aur_post_build
+def post_build():
+    aur_post_build()
+    del os.environ['http_proxy']
+    del os.environ['https_proxy']
 
 if __name__ == '__main__':
     single_main(build_prefix)
