6f3d1c6d87ee3980bedaff4a6de3180ffab3b08a
Jingbei Li <i@jingbei.li>
2019-07-24 15:09:14 +0800

    added r-mkl for x86_64

---
+++ r-mkl/lilac.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from lilaclib import *
+
+maintainers = [{'github': 'petronny'}]
+update_on = [{'aur': None}]
+repo_depends = [('intel-parallel-studio-xe', 'intel-common-libs'), ('intel-parallel-studio-xe', 'intel-mkl'), ('intel-parallel-studio-xe', 'intel-openmp')]
+build_prefix = 'extra-x86_64'
+pre_build = aur_pre_build
+post_build = aur_post_build
+
+if __name__ == '__main__':
+    single_main(build_prefix)
