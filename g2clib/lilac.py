b87fc3db9176631573f001f07288bbfe2a1bb82d
Winston Wu <winstonwu91@gmail.com>
2019-07-28 10:09:23 -0400

    Modify lilac.py for g2clib

--- g2clib/lilac.py
+++ g2clib/lilac.py
@@ -4,7 +4,15 @@ from lilaclib import *
 maintainers = [{'github': 'heavysink'}]
 update_on = [{'aur': None}]
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
+def pre_build(): 
+    aur_pre_build()
+
+    for line in edit_file('PKGBUILD'):
+        if line.startswith('dependes=('):
+            print(line.replace('dependes', 'depends'))
+        else:
+            print(line)
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
