63b26cdfa26f633f793436659445820916178310
Winston Wu <winstonwu91@gmail.com>
2019-07-29 10:11:58 -0400

    addpkg: ncl

---
+++ ncl/lilac.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from lilaclib import *
+
+maintainers = [{'github': 'heavysink', 'email': 'Heavysink <winstonwu91@gmail.com>'}]
+update_on = [{'aur': None}]
+build_prefix = 'extra-x86_64'
+def pre_build(): 
+    aur_pre_build()
+
+    for line in edit_file('PKGBUILD'):
+        print(line.replace('-fopenmp', '-fopenmp -DACCEPT_USE_OF_DEPRECATED_PROJ_API_H '))
+
+post_build = aur_post_build
+
+if __name__ == '__main__':
+    single_main(build_prefix)
