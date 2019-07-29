360ca6aab57873b5eb9980dc30964406ed4d8ac9
Winston Wu <winstonwu91@gmail.com>
2019-07-29 23:55:30 +0000

    Repair archimedes-tools and ncl

--- archimedes-tools/lilac.py
+++ archimedes-tools/lilac.py
@@ -2,7 +2,7 @@
 from lilaclib import *
 
 maintainers = [{'github': 'heavysink', 'email': 'Heavysink <winstonwu91@gmail.com>'}]
-update_on = [{'aur': None}]
+update_on = [{'aur': showme}]
 build_prefix = 'extra-x86_64'
 pre_build = aur_pre_build
 post_build = aur_post_build
--- ncl/lilac.py
+++ ncl/lilac.py
@@ -3,12 +3,16 @@ from lilaclib import *
 
 maintainers = [{'github': 'heavysink', 'email': 'Heavysink <winstonwu91@gmail.com>'}]
 update_on = [{'aur': None}]
+repo_depends = ['triangle', 'udunits','g2clib']
 build_prefix = 'extra-x86_64'
-def pre_build(): 
+def pre_build():
     aur_pre_build()
 
     for line in edit_file('PKGBUILD'):
-        print(line.replace('-fopenmp', '-fopenmp -DACCEPT_USE_OF_DEPRECATED_PROJ_API_H '))
+        if line.startswith('depends=('):
+            print(line.replace('lesstif','openmotif'))
+        else:
+            print(line.replace('-fopenmp', '-fopenmp -DACCEPT_USE_OF_DEPRECATED_PROJ_API_H '))
 
 post_build = aur_post_build
 
