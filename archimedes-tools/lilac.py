55426ce96b03ce89d2e1b97470243b98d12d8f06
Winston Wu <winstonwu91@gmail.com>
2019-08-05 15:59:02 +0000

    Repair ncl and archimedes-tools

--- archimedes-tools/lilac.py
+++ archimedes-tools/lilac.py
@@ -2,7 +2,7 @@
 from lilaclib import *
 
 maintainers = [{'github': 'heavysink', 'email': 'Heavysink <winstonwu91@gmail.com>'}]
-update_on = [{'aur': showme}]
+update_on = [{'aur': 'triangle'}]
 build_prefix = 'extra-x86_64'
 pre_build = aur_pre_build
 post_build = aur_post_build
