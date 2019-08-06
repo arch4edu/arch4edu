035f010271a68187dfd1b4260ea9fc5898d23b60
Winston Wu <winstonwu91@gmail.com>
2019-08-06 14:44:24 +0000

    Repair lilac.py at ncl again...

--- ncl/lilac.py
+++ ncl/lilac.py
@@ -9,9 +9,7 @@ def pre_build():
     aur_pre_build()
 
     for line in edit_file('PKGBUILD'):
-        if line.startswith('depends=('):
             print(line.replace('lesstif','openmotif'))
-        else:
             print(line.replace('-fopenmp', '-fopenmp -DACCEPT_USE_OF_DEPRECATED_PROJ_API_H '))
 
 post_build = aur_post_build
