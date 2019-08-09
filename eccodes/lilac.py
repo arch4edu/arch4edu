99df3264b89e172e142a06095a8acbe0f6484305
Winston Wu <winstonwu91@gmail.com>
2019-08-09 00:30:30 -0400

    addpkg: eccodes

---
+++ eccodes/lilac.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from lilaclib import *
+
+maintainers = [{'github': 'heavysink', 'email': 'Heavysink <winstonwu91@gmail.com>'}]
+update_on = [{'aur': None}]
+build_prefix = 'extra-x86_64'
+def pre_build():
+    aur_pre_build()
+    add_makedepends(['jasper'])
+
+post_build = aur_post_build
+
+if __name__ == '__main__':
+    single_main(build_prefix)
