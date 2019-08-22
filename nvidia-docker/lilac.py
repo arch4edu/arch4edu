7a75678c196518761b4c0c41d9fcb3986966bdba
Jingbei Li <i@jingbei.li>
2019-08-22 22:12:45 +0800

    removed nvidia-container-runtime-hook for x86_64

--- nvidia-docker/lilac.py
+++ nvidia-docker/lilac.py
@@ -4,7 +4,7 @@ from lilaclib import *
 maintainers = [{'github': 'petronny'}]
 update_on = [{'aur': None}]
 build_prefix = 'extra-x86_64'
-repo_depends = ['nvidia-container-runtime', 'nvidia-container-runtime-hook']
+repo_depends = ['nvidia-container-runtime']
 pre_build = aur_pre_build
 post_build = aur_post_build
 
