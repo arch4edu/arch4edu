7700910e33e89a4b5edcbfb4fe96fcc8ff8c4285
Jingbei Li <i@jingbei.li>
2019-08-23 11:50:47 +0800

    added python-cheetah3 for x86_64

--- python-cheetah3/lilac.py
+++ python-cheetah3/lilac.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 from lilaclib import *
-import os
 
 maintainers = [{'github': 'petronny'}]
 update_on = [{'aur': None}]
 build_prefix = 'extra-x86_64'
-repo_depends = ['python-cheetah3', 'python2-scons', 'wiredtiger']
-build_args = ['-r', os.path.expanduser('~/.lilac/archbuild')]
-time_limit_hours = 4
 pre_build = aur_pre_build
 post_build = aur_post_build
 
