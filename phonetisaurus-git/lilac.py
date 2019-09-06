d74847d663471173342f89093d67c7c2b38fff51
Jingbei Li <i@jingbei.li>
2019-09-06 14:45:35 +0800

    fixed phonetisaurus-git/lilac.py

--- phonetisaurus-git/lilac.py
+++ phonetisaurus-git/lilac.py
@@ -4,7 +4,7 @@ from lilaclib import *
 maintainers = [{'github': 'petronny'}]
 update_on = [{'aur': None}, {'github': 'AdolfVonKleist/Phonetisaurus'}]
 build_prefix = 'extra-x86_64'
-repo_depends = ['openfst']
+repo_depends = ['openfst', 'python-pybindgen']
 pre_build = aur_pre_build
 post_build = aur_post_build
 
