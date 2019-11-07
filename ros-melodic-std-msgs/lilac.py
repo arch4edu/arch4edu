13f8f33b60d572060484dd746da698d8569dd2f8
Jingbei Li <i@jingbei.li>
2019-11-07 15:22:08 +0800

    added ros-melodic-std-msgs for any #44

---
+++ ros-melodic-std-msgs/lilac.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from lilaclib import *
+
+maintainers = [{'github': 'petronny'}]
+update_on = [{'aur': None}]
+build_prefix = 'extra-x86_64'
+repo_depends = ['ros-build-tools-py3', 'ros-melodic-catkin', 'ros-melodic-message-generation', 'ros-melodic-message-runtime']
+pre_build = aur_pre_build
+post_build = aur_post_build
+
+if __name__ == '__main__':
+    single_main(build_prefix)
