21dd290f675c3a753034eccd7e67c8b238cfc7d3
Jingbei Li <i@jingbei.li>
2019-11-07 15:02:52 +0800

    added ros-melodic-message-generation for any #44

---
+++ ros-melodic-message-generation/lilac.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from lilaclib import *
+
+maintainers = [{'github': 'petronny'}]
+update_on = [{'aur': None}]
+build_prefix = 'extra-x86_64'
+repo_depends = ['ros-build-tools-py3', 'ros-melodic-catkin', 'ros-melodic-gencpp', 'ros-melodic-geneus', 'ros-melodic-genlisp', 'ros-melodic-genmsg', 'ros-melodic-gennodejs', 'ros-melodic-genpy']
+pre_build = aur_pre_build
+post_build = aur_post_build
+
+if __name__ == '__main__':
+    single_main(build_prefix)
