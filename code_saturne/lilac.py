811a0cf36ffbbf41119cf8181cfa49c22c5fef34
Jingbei Li <i@jingbei.li>
2019-11-18 12:50:47 +0800

    fixed code_saturne/lilac.py

--- code_saturne/lilac.py
+++ code_saturne/lilac.py
@@ -3,6 +3,7 @@ from lilaclib import *
 
 maintainers = [{'github': 'heavysink', 'email': 'Heavysink <winstonwu91@gmail.com>'}, {'github': 'petronny'}]
 update_on = [{'aur': None}, {'alias': 'python'}]
+repo_depends = ['med', 'parmetis', 'scotch']
 build_prefix = 'extra-x86_64'
 pre_build = aur_pre_build
 post_build = aur_post_build
