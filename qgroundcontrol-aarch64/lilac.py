fa7ae76f9369e0bf3ff3d60c73104ec4a1f5b690
Jingbei Li <i@jingbei.li>
2019-11-20 13:01:18 +0800

    splitted qgroundcontrol into 2 directories

---
+++ qgroundcontrol-aarch64/lilac.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from lilaclib import *
+
+maintainers = [{'github': 'petronny'}]
+update_on = [{'aur': None}]
+build_prefix = ['extra-x86_64', 'extra-aarch64']
+time_limit_hours = 4
+
+def pre_build():
+    aur_pre_build()
+    add_arch(['armv6h', 'armv7h', 'aarch64'])
+
+    for line in edit_file('PKGBUILD'):
+        if line.startswith('\tpatch'):
+            print('\tsed "s|so.56|so.*|g" -i ${srcdir}/${pkgname}-${pkgver}/QGCSetup.pri')
+        else:
+            print(line)
+
+post_build = aur_post_build
+
+if __name__ == '__main__':
+    single_main('extra-x86_64')
--- qgroundcontrol/lilac.py
+++ qgroundcontrol/lilac.py
@@ -3,19 +3,9 @@ from lilaclib import *
 
 maintainers = [{'github': 'petronny'}]
 update_on = [{'aur': None}]
-build_prefix = ['extra-x86_64', 'extra-aarch64']
+build_prefix = 'extra-x86_64'
 time_limit_hours = 4
-
-def pre_build():
-    aur_pre_build()
-    add_arch(['armv6h', 'armv7h', 'aarch64'])
-
-    for line in edit_file('PKGBUILD'):
-        if line.startswith('\tpatch'):
-            print('\tsed "s|so.56|so.*|g" -i ${srcdir}/${pkgname}-${pkgver}/QGCSetup.pri')
-        else:
-            print(line)
-
+pre_build = aur_pre_build
 post_build = aur_post_build
 
 if __name__ == '__main__':
