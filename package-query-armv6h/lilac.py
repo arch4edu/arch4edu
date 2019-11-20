d748016caba8729a3aba640dcf7c429fa46cef48
Jingbei Li <i@jingbei.li>
2019-11-20 13:26:24 +0800

    splitted package-query into 4 directories

--- package-query-armv6h/lilac.py
+++ package-query-armv6h/lilac.py
@@ -3,7 +3,7 @@ from lilaclib import *
 
 maintainers = [{'github': 'petronny'}]
 update_on = [{'aur': None}]
-build_prefix = 'extra-x86_64'
+build_prefix = 'extra-armv6h'
 pre_build = aur_pre_build
 post_build = aur_post_build
 
