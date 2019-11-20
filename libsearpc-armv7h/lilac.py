e41a2f6ff9bb3b00c809b79ae00c659f60d6d2a0
Jingbei Li <i@jingbei.li>
2019-11-20 13:33:23 +0800

    splitted libsearpc into 4 directories

--- libsearpc-armv7h/lilac.py
+++ libsearpc-armv7h/lilac.py
@@ -3,7 +3,7 @@ from lilaclib import *
 
 maintainers = [{'github': 'petronny'}]
 update_on = [{'aur': None}]
-build_prefix = 'extra-x86_64'
+build_prefix = 'extra-armv7h'
 pre_build = aur_pre_build
 post_build = aur_post_build
 
