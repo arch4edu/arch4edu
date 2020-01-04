694ff57cef3ec0a39772518f0317711b3ad47e99
Jingbei Li <i@jingbei.li>
2020-01-04 21:48:03 +0800

    fixed pid_children-git/lilac.py

--- pid_children-git/lilac.py
+++ pid_children-git/lilac.py
@@ -1,11 +1,17 @@
-#!/usr/bin/env python3
+#!/bin/python3
 from lilaclib import *
 
 maintainers = [{'github': 'petronny'}]
 update_on = [{'github': 'lilydjwg/pid_children'}]
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
-post_build = aur_post_build
+
+def pre_build():
+    run_cmd(['sh', 'download-files-from-github.sh', 'archlinuxcn/repo', 'archlinuxcn/pid_children-git'])
+    run_cmd(['git', 'rm', '-f', 'lilac.yaml'])
+
+def post_build():
+    git_add_files('PKGBUILD')
+    git_commit()
 
 if __name__ == '__main__':
     single_main(build_prefix)
