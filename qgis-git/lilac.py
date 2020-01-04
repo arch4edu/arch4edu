8be8fb0b8fe489888717428d4c2cb5cf6decb326
Jingbei Li <i@jingbei.li>
2020-01-04 23:14:00 +0800

    increased time limit for qgis-git

--- qgis-git/lilac.py
+++ qgis-git/lilac.py
@@ -6,6 +6,7 @@ update_on = [{'aur': None}, {'github': 'qgis/QGIS'}]
 build_prefix = 'extra-x86_64'
 pre_build = aur_pre_build
 post_build = aur_post_build
+time_limit_hours = 5
 
 if __name__ == '__main__':
     single_main(build_prefix)
