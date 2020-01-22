5dc0c7cb98a75c1a9f65481660ab2da80d035100
Jingbei Li <i@jingbei.li>
2020-01-22 12:44:40 +0800

    added python3-memoizedb for any

--- python3-memoizedb/lilac.py
+++ python3-memoizedb/lilac.py
@@ -3,7 +3,6 @@ from lilaclib import *
 
 maintainers = [{'github': 'petronny'}]
 update_on = [{'aur': None}, {'alias': 'python'}]
-repo_depends = ['python3-memoizedb', 'python3-xcgf']
 build_prefix = 'extra-x86_64'
 pre_build = aur_pre_build
 post_build = aur_post_build
