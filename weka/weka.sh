f0732514de85c361939eda4887f316305fc19a9b
calil <calil@jingbei.li>
2020-02-04 03:25:23 +0800

    auto update for package weka

--- weka/weka.sh
+++ weka/weka.sh
@@ -1,16 +1,9 @@
-#! /bin/sh
+#!/usr/bin/sh
 
 # Note: the '-Xmx2g' flag is used to set the maximum memory that the java
 # program is allowed. When working with large data sets, this number may need
 # to be increased based on how much memory you wish to allow weka to have.
 # Specifying command line arguments will override the defaults shown here.
 # Example: '-Xmx4g'
-DEFAULT_ARGS='-Xms32m -Xmx2g'
 
-if [ $# -gt 0 ]; then
-	ARGS="$@"
-else
-	ARGS="$DEFAULT_ARGS"
-fi
-
-java $ARGS -jar /usr/share/java/weka/weka.jar
+exec /usr/bin/java -Xms32m -Xmx2g "$@" -jar /usr/share/weka/weka.jar
