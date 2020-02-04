635e3b5409ed6f678d9378e833b662e2086bce7c
Jingbei Li <i@jingbei.li>
2020-02-04 12:27:17 +0800

    added pm2ml for any

--- pm2ml/lilac.py
+++ pm2ml/lilac.py
@@ -3,7 +3,7 @@ from lilaclib import *
 
 maintainers = [{'github': 'petronny'}]
 update_on = [{'aur': None}, {'alias': 'python'}]
-repo_depends = ['pm2ml', 'python3-xcgf', 'python3-xcpf']
+repo_depends = ['python3-xcgf', 'python3-xcpf']
 build_prefix = 'extra-x86_64'
 pre_build = aur_pre_build
 post_build = aur_post_build
