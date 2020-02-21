de823d5984ff55cf49ae298453954098bee8eafd
Jingbei Li <i@jingbei.li>
2020-02-21 15:37:39 +0800

    removed intel-dnnl moved to [community]

--- mxnet/lilac.py
+++ mxnet/lilac.py
@@ -4,7 +4,7 @@ from lilaclib import *
 maintainers = [{'github': 'petronny'}]
 update_on = [{'aur': None}, {'archpkg': 'cuda'}, {'archpkg': 'opencv'}, {'alias': 'python'}]
 build_prefix = 'extra-x86_64'
-repo_depends = ['gcc7', ('gcc7', 'gcc7-libs'), 'python-graphviz', ('intel-parallel-studio-xe', 'intel-common-libs'), ('intel-parallel-studio-xe', 'intel-compiler-base'), ('intel-parallel-studio-xe', 'intel-mkl'), ('intel-parallel-studio-xe', 'intel-openmp'), 'intel-dnnl']
+repo_depends = ['gcc7', ('gcc7', 'gcc7-libs'), 'python-graphviz', ('intel-parallel-studio-xe', 'intel-common-libs'), ('intel-parallel-studio-xe', 'intel-compiler-base'), ('intel-parallel-studio-xe', 'intel-mkl'), ('intel-parallel-studio-xe', 'intel-openmp')]
 makechrootpkg_args = ['-D', '/opt/intel/licenses']
 time_limit_hours = 4
 pre_build = aur_pre_build
