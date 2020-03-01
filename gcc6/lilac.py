969ccb4e327e305f21ccf0591265827baac05951
Jingbei Li <i@jingbei.li>
2020-03-01 12:46:23 +0800

    fixed gcc6/lilac.py

--- gcc6/lilac.py
+++ gcc6/lilac.py
@@ -3,7 +3,7 @@ from lilaclib import *
 
 maintainers = [{'github': 'petronny'}]
 update_on = [{'aur': None}]
-build_prefix = ['extra-x86_64']
+build_prefix = 'extra-x86_64'
 time_limit_hours = 8
 pre_build = aur_pre_build
 post_build = aur_post_build
