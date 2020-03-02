69a672857e14ac6ece8b4fc5c6b49abedfe1025c
Jingbei Li <i@jingbei.li>
2020-03-02 15:27:07 +0800

    fixed python-property-cached/lilac.py

--- python-property-cached/lilac.py
+++ python-property-cached/lilac.py
@@ -6,6 +6,7 @@ update_on = [{'aur': None}, {'alias': 'python'}]
 build_prefix = 'extra-x86_64'
 pre_build = aur_pre_build
 post_build = aur_post_build
+makepkg_args = ['--nocheck']
 
 if __name__ == '__main__':
     single_main(build_prefix)
