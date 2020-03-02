7630c1887930a483e56bb19991ce937ce2389b40
Jingbei Li <i@jingbei.li>
2020-03-02 18:24:59 +0800

    increased time limit for opencv-cuda

--- opencv-cuda/lilac.py
+++ opencv-cuda/lilac.py
@@ -6,6 +6,7 @@ update_on = [{'aur': None}]
 build_prefix = 'arch4edu-x86_64'
 pre_build = aur_pre_build
 post_build = aur_post_build
+time_limit_hours = 4
 
 if __name__ == '__main__':
     single_main(build_prefix)
