3601cbe0c28ba683f7788d8c97b618461190c5d9
Jingbei Li <i@jingbei.li>
2020-03-05 15:45:07 +0800

    added ttf-{charis-sil,sil-doulos} for any

--- ttf-charis-sil/lilac.py
+++ ttf-charis-sil/lilac.py
@@ -3,7 +3,6 @@ from lilaclib import *
 
 maintainers = [{'github': 'petronny'}]
 update_on = [{'aur': None}]
-repo_depends = ['ttf-charis-sil', 'ttf-sil-doulos']
 build_prefix = 'extra-x86_64'
 pre_build = aur_pre_build
 post_build = aur_post_build
