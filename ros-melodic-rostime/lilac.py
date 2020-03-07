74b0f50546dea90747b2c66ea83e9cfa021a469c
Jingbei Li <i@jingbei.li>
2020-03-07 12:53:17 +0800

    fixed ros-melodic-{cpp-common,rostime}/lilac.py #59

--- ros-melodic-rostime/lilac.py
+++ ros-melodic-rostime/lilac.py
@@ -2,7 +2,7 @@
 from lilaclib import *
 
 maintainers = [{'github': 'petronny'}]
-update_on = [{'aur': None}]
+update_on = [{'aur': None}, {'alias': 'boost'}]
 build_prefix = 'extra-x86_64'
 repo_depends = ['ros-build-tools-py3', 'ros-melodic-catkin', 'ros-melodic-cpp-common']
 pre_build = aur_pre_build
