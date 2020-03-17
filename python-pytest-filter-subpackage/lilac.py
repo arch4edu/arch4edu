03c9da73c1916f2aa1b072be3ce8dcf7d1fe1689
Jingbei Li <i@jingbei.li>
2020-03-17 16:16:39 +0800

    added python-pytest-filter-subpackage for x86_64

--- python-pytest-filter-subpackage/lilac.py
+++ python-pytest-filter-subpackage/lilac.py
@@ -4,7 +4,6 @@ from lilaclib import *
 maintainers = [{'github': 'petronny'}]
 update_on = [{'aur': None}, {'alias': 'python'}]
 build_prefix = 'extra-x86_64'
-repo_depends = ['python-pytest-astropy-header', 'python-pytest-doctestplus', 'python-pytest-remotedata', 'python-pytest-openfiles', 'python-pytest-arraydiff', 'python-pytest-filter-subpackage']
 pre_build = aur_pre_build
 post_build = aur_post_build
 
