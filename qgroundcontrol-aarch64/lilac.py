76f7a2c19862e8b774ff9007c855cb8d1d844d7a
Jingbei Li <i@jingbei.li>
2020-03-21 12:55:35 +0800

    fixed qgroundcontrol{,-aarch64}/lilac.py

--- qgroundcontrol-aarch64/lilac.py
+++ qgroundcontrol-aarch64/lilac.py
@@ -7,9 +7,10 @@ build_prefix = 'extra-aarch64'
 time_limit_hours = 4
 
 def pre_build():
+    run_cmd(['rm', '*.part'])
     aur_pre_build('qgroundcontrol')
     add_arch(['aarch64'])
-    add_makedepends(['qt5-x11extras', 'qt5-wayland'])
+    add_makedepends(['qt5-tools', 'qt5-x11extras', 'qt5-wayland'])
 
 post_build = aur_post_build
 
--- qgroundcontrol/lilac.py
+++ qgroundcontrol/lilac.py
@@ -7,8 +7,9 @@ build_prefix = 'extra-x86_64'
 time_limit_hours = 4
 
 def pre_build():
+    run_cmd(['rm', '*.part'])
     aur_pre_build()
-    add_makedepends(['qt5-x11extras', 'qt5-wayland'])
+    add_makedepends(['qt5-tools', 'qt5-x11extras', 'qt5-wayland'])
 
 post_build = aur_post_build
 
