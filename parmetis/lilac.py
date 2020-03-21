c9da8a88bfd6b55b68d51b85de7fc5fbad9062bb
Jingbei Li <i@jingbei.li>
2020-03-21 12:58:49 +0800

    fixed parmetis/lilac.py

--- parmetis/lilac.py
+++ parmetis/lilac.py
@@ -4,7 +4,16 @@ from lilaclib import *
 maintainers = [{'github': 'petronny'}]
 update_on = [{'aur': None}]
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
+
+def pre_build():
+    aur_pre_build()
+
+    for line in edit_file('PKGBUILD'):
+        if line.startswith('_pkgdirname='):
+            print(line.replace('73dab469aa36', '475d8facbb32'))
+        else:
+            print(line)
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
