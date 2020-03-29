900172f168e5c0002b08dfc49f90aa2492f60337
Jingbei Li <i@jingbei.li>
2020-03-29 11:49:13 +0800

    added my email to archrepo2-git/lilac.py

--- archrepo2-git/lilac.py
+++ archrepo2-git/lilac.py
@@ -1,7 +1,7 @@
 #!/bin/python3
 from lilaclib import *
 
-maintainers = [{'github': 'petronny'}]
+maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
 update_on = [{'github': 'archlinuxcn/repo', 'path': 'archlinuxcn/archrepo2-git/PKGBUILD'}, {'github': 'lilydjwg/archrepo2'}]
 build_prefix = 'extra-x86_64'
 
