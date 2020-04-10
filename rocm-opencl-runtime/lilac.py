9cd3a81eca75b4600dbb4ca2bae8f69be1904ef0
Jingbei Li <i@jingbei.li>
2020-04-10 16:17:41 +0800

    replaced rocm-runtime in lilac.py

--- rocm-opencl-runtime/lilac.py
+++ rocm-opencl-runtime/lilac.py
@@ -3,7 +3,7 @@ from lilaclib import *
 
 maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
 update_on = [{'aur': None}]
-repo_depends = ['hsakmt-roct', 'llvm-roc', 'rocm-comgr', 'rocr-runtime']
+repo_depends = ['hsakmt-roct', 'llvm-roc', 'rocm-comgr', 'hsa-rocr']
 build_prefix = 'extra-x86_64'
 pre_build = aur_pre_build
 post_build = aur_post_build
