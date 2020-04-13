55a4900f89b15a6f38f62ad03c5ba24c89ae952a
Jingbei Li <i@jingbei.li>
2020-04-13 18:17:15 +0800

    added hsa-ext-rocr for x86_64

--- hsa-ext-rocr/lilac.py
+++ hsa-ext-rocr/lilac.py
@@ -3,7 +3,7 @@ from lilaclib import *
 
 maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
 update_on = [{'aur': None}]
-repo_depends = ['rocm-cmake', 'hsa-rocr', 'hsakmt-roct', 'hsa-ext-rocr']
+repo_depends = ['hsa-rocr', 'hsakmt-roct']
 build_prefix = 'extra-x86_64'
 pre_build = aur_pre_build
 post_build = aur_post_build
