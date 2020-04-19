80968a4f4fc51ddfb17e433cac741821b664e59b
Jingbei Li <i@jingbei.li>
2020-04-19 15:54:33 +0800

    fixed heasoft/lilac.py #66

--- heasoft/lilac.py
+++ heasoft/lilac.py
@@ -4,6 +4,7 @@ from lilaclib import *
 maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
 update_on = [{'aur': None}]
 build_prefix = 'extra-x86_64'
+time_limit_hours = 4
 
 def pre_build():
     aur_pre_build()
