282c89f6beb1386c69855e8ab5aa4cb6254bda64
Jingbei Li <i@jingbei.li>
2020-04-20 14:49:02 +0800

    fixed rocr-debug-agent/lilac.py

--- rocr-debug-agent/lilac.py
+++ rocr-debug-agent/lilac.py
@@ -3,7 +3,7 @@ from lilaclib import *
 
 maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
 update_on = [{'aur': None}]
-repo_depends = ['hsa-rocr', 'hsakmt-roct']
+repo_depends = ['hsa-rocr', 'hsakmt-roct', 'rocm-clang-ocl']
 build_prefix = 'extra-x86_64'
 pre_build = aur_pre_build
 post_build = aur_post_build
