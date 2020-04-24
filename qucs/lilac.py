2016e7d87f443b98c1e745e2b91b7cb86738e4c3
Jingbei Li <i@jingbei.li>
2020-04-24 13:18:11 +0800

    fixed qucs/lilac.py

--- qucs/lilac.py
+++ qucs/lilac.py
@@ -4,7 +4,7 @@ from lilaclib import *
 maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
 update_on = [{'aur': None}]
 build_prefix = 'extra-x86_64'
-repo_depends = ['adms']
+repo_depends = ['adms', 'qt4']
 pre_build = aur_pre_build
 post_build = aur_post_build
 
