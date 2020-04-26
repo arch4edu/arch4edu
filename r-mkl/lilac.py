9e657f88c9fe7417d480e21c55876d1e9f4979e5
Jingbei Li <i@jingbei.li>
2020-04-26 12:00:20 +0800

    fixed r-mkl/lilac.py

--- r-mkl/lilac.py
+++ r-mkl/lilac.py
@@ -2,8 +2,7 @@
 from lilaclib import *
 
 maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
-update_on = [{'aur': None}, {'aur': 'intel-mkl'}, {'alias': 'icu'}]
-repo_depends = [('intel-parallel-studio-xe', 'intel-common-libs'), ('intel-parallel-studio-xe', 'intel-mkl'), ('intel-parallel-studio-xe', 'intel-openmp')]
+update_on = [{'aur': None}, {'archpkg': 'intel-mkl'}, {'alias': 'icu'}]
 build_prefix = 'extra-x86_64'
 pre_build = aur_pre_build
 post_build = aur_post_build
