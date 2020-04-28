181fcbc5644efbc4ed76a9037d21e308c725a658
Jingbei Li <i@jingbei.li>
2020-04-28 13:06:59 +0800

    fixed python-pysynphot/lilac.py #71

--- python-pysynphot/lilac.py
+++ python-pysynphot/lilac.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 from lilaclib import *
 
-maintainers = [{'github': 'migueldvb', 'email': 'Miguel de Val-Borro <miguel.deval@gmail.com>'}]
-update_on = [{'aur': None}]
+maintainers = [
+        {'github': 'migueldvb', 'email': 'Miguel de Val-Borro <miguel.deval@gmail.com>'},
+        {'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
+update_on = [{'aur': None}, {'alias': 'python'}]
+repo_depends = ['python-astropy']
 build_prefix = 'extra-x86_64'
 pre_build = aur_pre_build
 post_build = aur_post_build
