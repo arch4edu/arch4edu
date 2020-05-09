df04deeed0475084fd879291f99c30dfe1cce237
Jingbei Li <i@jingbei.li>
2020-05-09 16:13:29 +0800

    fixed scilab-bin/lilac.py

--- scilab-bin/lilac.py
+++ scilab-bin/lilac.py
@@ -3,6 +3,7 @@ from lilaclib import *
 
 maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
 update_on = [{'aur': None}]
+repo_depends = ['ncurses5-compat-libs']
 build_prefix = 'extra-x86_64'
 pre_build = aur_pre_build
 post_build = aur_post_build
