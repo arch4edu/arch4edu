6a277a8bb9ca899863d00fcd4e156287c45e9fc5
Jingbei Li <i@jingbei.li>
2020-05-09 12:28:33 +0800

    fixed gtsam{,-mkl}/lilac.py

--- gtsam/lilac.py
+++ gtsam/lilac.py
@@ -4,11 +4,7 @@ from lilaclib import *
 maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
 update_on = [{'aur': None}]
 build_prefix = 'extra-x86_64'
-
-def pre_build():
-    aur_pre_build()
-    add_depends(['eigen'])
-
+pre_build = aur_pre_build
 post_build = aur_post_build
 
 if __name__ == '__main__':
