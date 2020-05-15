dc1531184cfe209ff95b27431c31ae27135ed7c3
heavysink <winstonwu91@gmail.com>
2020-05-15 11:53:03 -0400

    addpkg: mumps and mumps-par

---
+++ mumps/lilac.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from lilaclib import *
+
+maintainers = [{'github': 'heavysink', 'email': 'Heavysink <winstonwu91@gmail.com>'}]
+update_on = [{'aur': None}]
+repo_depends = ['scotch','scalapack','parmetis']
+build_prefix = 'extra-x86_64'
+def pre_build():
+    aur_pre_build()
+    for line in edit_file('PKGBUILD'):
+        if 'alllib' in line:
+            print('export CFLAGS+=" -fallow-argument-mismatch"')
+            print(line)
+        else:
+            print(line)
+
+post_build = aur_post_build
+
+if __name__ == '__main__':
+    single_main(build_prefix)
