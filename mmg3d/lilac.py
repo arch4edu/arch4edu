cc1337b6786ef19c8424f908b3217f24d6b45ab4
heavysink <winstonwu91@gmail.com>
2020-05-15 02:52:21 -0400

    addpkg: mmg3d

---
+++ mmg3d/lilac.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+from lilaclib import *
+
+maintainers = [{'github': 'heavysink', 'email': 'Heavysink <winstonwu91@gmail.com>'}]
+update_on = [{'aur': None}]
+repo_depends = ['scotch']
+build_prefix = 'extra-x86_64'
+def pre_build():
+    aur_pre_build()
+    for line in edit_file('PKGBUILD'):
+        if 'arch=' in line:
+            print(line)
+            print('depends=(scotch)')
+        elif 'mmg-$pkgver' in line:
+            print(line)
+            print('export CFLAGS+=" -fcommon"')
+            print('export CXXFLAGS+=" -fcommon"')
+        else:
+            print(line)
+
+post_build = aur_post_build
+
+if __name__ == '__main__':
+    single_main(build_prefix)
