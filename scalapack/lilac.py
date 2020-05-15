25c6af81c6e187bd82dbeb933b43b625a52c2feb
heavysink <winstonwu91@gmail.com>
2020-05-15 12:51:22 -0400

    [scalapack] Repair GCC 10 type mismatch

--- scalapack/lilac.py
+++ scalapack/lilac.py
@@ -4,7 +4,15 @@ from lilaclib import *
 maintainers = [{'github': 'heavysink', 'email': 'Heavysink <winstonwu91@gmail.com>'}, {'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
 update_on = [{'aur': None}]
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
+def pre_build():
+    aur_pre_build()
+    for line in edit_file('PKGBUILD'):
+        if 'cmake' in line:
+            print('export FFLAGS+=" -fallow-argument-mismatch"')
+            print(line)
+        else:
+            print(line)
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
