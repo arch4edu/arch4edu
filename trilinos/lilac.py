96e01ba4da9a50fe6fc0772401812d1177a205de
heavysink <winstonwu91@gmail.com>
2020-05-15 22:49:27 -0400

    [Trilinos] lilac.py add zoltan as provide and conflict

--- trilinos/lilac.py
+++ trilinos/lilac.py
@@ -5,23 +5,24 @@ maintainers = [{'github': 'heavysink', 'email': 'Heavysink <winstonwu91@gmail.co
 update_on = [{'aur': None}]
 build_prefix = 'extra-x86_64'
 def pre_build():
-	aur_pre_build()
-	add_depends(['gtest'])
-	for line in edit_file('PKGBUILD'):
-		if 'conflicts' in line:
-			print('')
-		elif 'cmake .. -DTrilinos' in line:
-			print('export FFLAGS+=" -fallow-argument-mismatch -fallow-invalid-boz"')
-			print('export CFLAGS+=" -fcommon"')
-			print(line)
-		elif 'make VERBOSE' in line:
-			print('make -j4')
-		elif 'make DESTDIR' in line:
-			print(line)
-			print('rm $pkgdir/usr/lib/libgtest.so')
-			print('rm -rf $pkgdir/usr/include/gtest')
-		else:
-			print(line)
+    aur_pre_build()
+    add_depends(['gtest'])
+    for line in edit_file('PKGBUILD'):
+        if 'conflicts' in line:
+            print('provides=(zoltan)')
+            print(line.replace('gtest','zoltan'))
+        elif 'cmake .. -DTrilinos' in line:
+            print('export FFLAGS+=" -fallow-argument-mismatch -fallow-invalid-boz"')
+            print('export CFLAGS+=" -fcommon"')
+            print(line)
+        elif 'make VERBOSE' in line:
+            print('make -j4')
+        elif 'make DESTDIR' in line:
+            print(line)
+            print('rm $pkgdir/usr/lib/libgtest.so')
+            print('rm -rf $pkgdir/usr/include/gtest')
+        else:
+            print(line)
 
 post_build = aur_post_build
 
