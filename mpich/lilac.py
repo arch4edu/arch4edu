50c50842a8a706ea4c632e633770b08370139cb4
heavysink <winstonwu91@gmail.com>
2020-05-16 11:16:30 -0400

    [mpich-arch4edu] Rename mpich to mpich-arch4edu for some improvement patches

--- mpich/lilac.py
+++ mpich/lilac.py
@@ -2,8 +2,14 @@
 from lilaclib import *
 
 maintainers = [{'github': 'heavysink', 'email': 'Heavysink <winstonwu91@gmail.com>'}]
-update_on = [{'aur': None}]
+update_on = [{'github': 'heavysink/aur', 'path': 'mpich-arch4edu'}]
 build_prefix = 'extra-x86_64'
+def pre_build():
+    run_cmd(['sh', 'download-files-from-github.sh', 'heavysink/aur', 'mpich-arch4edu'])
+
+def post_build():
+    git_add_files('PKGBUILD')
+    git_commit()
 
 if __name__ == '__main__':
     single_main(build_prefix)
