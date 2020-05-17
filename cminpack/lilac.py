e9fb2bcd18d694dbb1d4834c45a60133a313ab37
Jingbei Li <i@jingbei.li>
2020-05-17 11:59:22 +0800

    fixed cminpack/lilac.py

--- cminpack/lilac.py
+++ cminpack/lilac.py
@@ -4,7 +4,14 @@ from lilaclib import *
 maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
 update_on = [{'aur': None}]
 build_prefix = 'extra-x86_64'
-pre_build = aur_pre_build
+
+def pre_build():
+    aur_pre_build()
+
+    with open('PKGBUILD', 'a') as f:
+        f.write('export http_proxy=127.0.0.1:8123\n')
+        f.write('export https_proxy=127.0.0.1:8123\n')
+
 post_build = aur_post_build
 
 if __name__ == '__main__':
