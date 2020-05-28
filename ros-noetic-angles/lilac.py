17d128776a083b79322c90f07ace8e981f57b3ee
Jingbei Li <i@jingbei.li>
2020-05-28 12:13:25 +0800

    moved ros-melodic to ros-noetic #78

---
+++ ros-noetic-angles/lilac.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from lilaclib import *
+
+maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
+update_on = [{'aur': None}, {'alias': 'python'}]
+build_prefix = 'extra-x86_64'
+repo_depends = ['ros-build-tools-py3', 'ros-noetic-catkin']
+
+def pre_build():
+    aur_pre_build()
+
+    for line in edit_file('PKGBUILD'):
+        if "arch=('any')" in line:
+            print('arch=("x86_64")')
+        else:
+            print(line)
+
+post_build = aur_post_build
+
+if __name__ == '__main__':
+    single_main(build_prefix)
