ee8b636a050bdb859f9275421a566b66cb158f97
Jingbei Li <i@jingbei.li>
2020-05-31 23:45:42 +0800

    fixed ompl/lilac.py

--- ompl/lilac.py
+++ ompl/lilac.py
@@ -3,7 +3,6 @@ from lilaclib import *
 
 maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
 update_on = [{'aur': None}]
-repo_depends = ['ode']
 build_prefix = 'extra-x86_64'
 pre_build = aur_pre_build
 post_build = aur_post_build
