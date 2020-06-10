3e6dc57ade902ff456ec88c3c55a0108633b1168
Jingbei Li <i@jingbei.li>
2020-06-10 15:27:09 +0800

    added roctracer for x86_64 #60

--- roctracer/lilac.py
+++ roctracer/lilac.py
@@ -2,8 +2,8 @@
 from lilaclib import *
 
 maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
-update_on = [{'aur': None}, {'alias': 'python'}]
-repo_depends = ['python-cppheaderparser', 'hsa-rocr', 'roctracer']
+update_on = [{'aur': None}]
+repo_depends = ['hsa-rocr', 'python-cppheaderparser']
 build_prefix = 'extra-x86_64'
 pre_build = aur_pre_build
 post_build = aur_post_build
