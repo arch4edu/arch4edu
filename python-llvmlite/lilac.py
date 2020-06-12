70d7e3d65c5587b97c3789f72d16c3cc4fc54d49
Jingbei Li <i@jingbei.li>
2020-06-12 23:23:50 +0800

    fixed python-llvmlite/lilac.py

--- python-llvmlite/lilac.py
+++ python-llvmlite/lilac.py
@@ -3,7 +3,6 @@ from lilaclib import *
 
 maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
 update_on = [{'aur': None}, {'alias': 'python'}]
-repo_depends = ['llvm8', ('llvm8', 'llvm8-libs')]
 build_prefix = 'extra-x86_64'
 pre_build = aur_pre_build
 post_build = aur_post_build
