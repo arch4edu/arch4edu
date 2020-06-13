6e3398a70f967b9cf247f91e20350fca51788db8
Jingbei Li <i@jingbei.li>
2020-06-14 01:20:03 +0800

    use action to build vim-youcompleteme-git

--- vim-youcompleteme-git/lilac.py
+++ vim-youcompleteme-git/lilac.py
@@ -4,16 +4,17 @@ from lilaclib import *
 maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
 update_on = [{'aur': None}, {'github': 'Valloric/YouCompleteMe'}]
 repo_depends = ['ncurses5-compat-libs']
-build_prefix = 'extra-x86_64'
+build_prefix = 'action-extra-x86_64'
 
 def pre_build():
     aur_pre_build()
 
     for line in edit_file('PKGBUILD'):
         if not line.startswith('groups=('):
-            print(line.replace('http://download.eclipse.org', 'https://mirrors.tuna.tsinghua.edu.cn/eclipse'))
+            print(line)
 
 post_build = aur_post_build
 
 if __name__ == '__main__':
-    single_main(build_prefix)
+    from action_tools import action_main
+    action_main(build_prefix)
