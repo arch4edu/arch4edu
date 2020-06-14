4d666a03d3746c7ed87eac9a32c196a656e8b05e
Jingbei Li <i@jingbei.li>
2020-06-14 14:39:47 +0800

    use action to build kiwiirc

--- kiwiirc/lilac.py
+++ kiwiirc/lilac.py
@@ -3,7 +3,7 @@ from lilaclib import *
 
 maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
 update_on = [{'aur': None}, {'github': 'kiwiirc/kiwiirc'}]
-build_prefix = 'extra-x86_64'
+build_prefix = 'action-extra-x86_64'
 
 def pre_build():
     aur_pre_build(do_vcs_update=True)
@@ -11,4 +11,5 @@ def pre_build():
 post_build = aur_post_build
 
 if __name__ == '__main__':
-    single_main(build_prefix)
+    from action_tools import action_main
+    action_main(build_prefix)
