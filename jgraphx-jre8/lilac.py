1b259ba0f16670d80bfeb484928a271bbdbf4782
Jingbei Li <i@jingbei.li>
2020-06-14 17:36:37 +0800

    use action to build jgprahx-jre8

--- jgraphx-jre8/lilac.py
+++ jgraphx-jre8/lilac.py
@@ -3,9 +3,10 @@ from lilaclib import *
 
 maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
 update_on = [{'aur': None}]
-build_prefix = 'extra-x86_64'
+build_prefix = 'action-extra-x86_64'
 pre_build = aur_pre_build
 post_build = aur_post_build
 
 if __name__ == '__main__':
-    single_main(build_prefix)
+    from action_tools import action_main
+    action_main(build_prefix)
