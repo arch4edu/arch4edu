7400319f8507aebc3e8b2ba960c2fc317c7d37c1
Jingbei Li <i@jingbei.li>
2020-06-20 17:03:31 +0800

    fixed roctracer/lilac.py

--- roctracer/lilac.py
+++ roctracer/lilac.py
@@ -3,7 +3,7 @@ from lilaclib import *
 
 maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
 update_on = [{'aur': None}]
-repo_depends = ['hsa-rocr', 'python-cppheaderparser']
+repo_depends = ['hip-rocclr', 'python-cppheaderparser']
 build_prefix = 'extra-x86_64'
 pre_build = aur_pre_build
 post_build = aur_post_build
