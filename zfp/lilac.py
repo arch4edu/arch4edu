6671eb2c40f8ee5845f319cfa3a6860ca1e75a94
Jingbei Li <i@jingbei.li>
2020-06-23 14:39:46 +0800

    added new dependencies for python-scikit-image

--- zfp/lilac.py
+++ zfp/lilac.py
@@ -2,9 +2,8 @@
 from lilaclib import *
 
 maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
-update_on = [{'aur': None}, {'alias': 'python'}]
+update_on = [{'aur': None}]
 build_prefix = 'extra-x86_64'
-repo_depends = ['python-imageio', 'python-pywavelets', 'python-tifffile']
 pre_build = aur_pre_build
 post_build = aur_post_build
 
