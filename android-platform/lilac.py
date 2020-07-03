60879d6f9cddc2f026d47a84614cf723e9398f2a
Jingbei Li <i@jingbei.li>
2020-07-03 15:58:49 +0800

    use action to build android-platform

--- android-platform/lilac.py
+++ android-platform/lilac.py
@@ -3,26 +3,10 @@ from lilaclib import *
 
 maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
 update_on = [{'aur': None}]
-build_prefix = 'extra-x86_64'
-
-def pre_build():
-    aur_pre_build()
-
-    depends_line = ''
-    for line in edit_file('PKGBUILD'):
-        if line.startswith('depends=('):
-            depends_line = line
-        elif line.startswith('package()'):
-            print(line)
-            print('  %s' % depends_line)
-        else:
-            print(line)
-
-    with open('PKGBUILD', 'a') as f:
-        f.write('export http_proxy=127.0.0.1:8123\n')
-        f.write('export https_proxy=127.0.0.1:8123\n')
-
+build_prefix = 'action-multilib'
+pre_build = aur_pre_build
 post_build = aur_post_build
 
 if __name__ == '__main__':
-    single_main(build_prefix)
+    from action_tools import action_main
+    action_main(build_prefix)
