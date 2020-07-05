958a29770a1483d17df0ce78b13175b3a9bbfc75
Miguel de Val-Borro <miguel.deval@gmail.com>
2020-07-05 19:06:00 -0400

    added python-synphot for x86_64

---
+++ python-synphot/lilac.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from lilaclib import *
+
+maintainers = [
+        {'github': 'migueldvb', 'email': 'Miguel de Val-Borro <miguel.deval@gmail.com>'}
+        ]
+update_on = [{'aur': None}, {'alias': 'python'}]
+repo_depends = ['python-astropy', 'python-extension-helpers']
+build_prefix = 'extra-x86_64'
+pre_build = aur_pre_build
+post_build = aur_post_build
+
+if __name__ == '__main__':
+    single_main(build_prefix)
