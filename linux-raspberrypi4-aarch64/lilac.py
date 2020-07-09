97c7a779c9fad4924be6b3e1d59e787b052e47e7
Jingbei Li <i@jingbei.li>
2020-07-09 19:47:44 +0800

    trigger rebuild for linux-raspberrypi4

--- linux-raspberrypi4-aarch64/lilac.py
+++ linux-raspberrypi4-aarch64/lilac.py
@@ -4,14 +4,11 @@ from lilac2.pkgbuild import _get_package_version, get_srcinfo
 
 maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
 update_on = [{'github': 'moonman/MyPKGBUILDs', 'path': 'linux-raspberrypi4'}]
-build_prefix = 'action-skip'
+build_prefix = 'action-extra-aarch64'
+time_limit_hours = 3
 
 def pre_build():
     run_cmd(['sh', 'download-files-from-github.sh', 'moonman/MyPKGBUILDs', 'linux-raspberrypi4'])
-    add_arch(['x86_64'])
-    epoch, pkgver, pkgrel = _get_package_version(get_srcinfo())
-    run_cmd(['wget', '-nc', f'https://olegtown.pw/Public/ArchLinuxArm/RPi4/kernel/linux-raspberrypi4-{pkgver}-{pkgrel}-aarch64.pkg.tar.xz'])
-    run_cmd(['wget', '-nc', f'https://olegtown.pw/Public/ArchLinuxArm/RPi4/kernel/linux-raspberrypi4-headers-{pkgver}-{pkgrel}-aarch64.pkg.tar.xz'])
 
 def post_build():
     git_commit()
