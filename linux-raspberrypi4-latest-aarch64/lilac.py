b69c2189ca6c68d76025728e1e203a6339ccce85
Jingbei Li <i@jingbei.li>
2020-07-16 12:12:19 +0800

    added linux-raspberrypi4-latest for aarch64

---
+++ linux-raspberrypi4-latest-aarch64/lilac.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+from lilaclib import *
+from lilac2.pkgbuild import _get_package_version, get_srcinfo
+
+maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
+update_on = [{'github': 'moonman/MyPKGBUILDs', 'path': 'linux-raspberrypi4-latest'}]
+build_prefix = 'action-extra-aarch64'
+time_limit_hours = 3
+
+def pre_build():
+    run_cmd(['sh', 'download-files-from-github.sh', 'moonman/MyPKGBUILDs', 'linux-raspberrypi4-latest'])
+
+def post_build():
+    git_commit()
+
+if __name__ == '__main__':
+    from action_tools import action_main
+    action_main(build_prefix)
