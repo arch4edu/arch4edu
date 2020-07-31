94302cbfb42d1e73498c0487f3906bbebb27b863
Jingbei Li <i@jingbei.li>
2020-07-31 13:48:28 +0800

    fixed kaldi/lilac.py

--- kaldi/lilac.py
+++ kaldi/lilac.py
@@ -4,16 +4,10 @@ from lilaclib import *
 maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
 update_on = [{'aur': None}, {'github': 'kaldi-asr/kaldi'}]
 build_prefix = 'extra-x86_64'
-repo_depends = ['cub', 'gcc7', ('gcc7', 'gcc7-libs'), 'kaldi-openfst', 'openblas-lapack']
+repo_depends = ['cub', 'kaldi-openfst', 'openblas-lapack']
 
 def pre_build():
     aur_pre_build(do_vcs_update=True)
-
-    for line in edit_file('PKGBUILD'):
-        if 'makedepends=(' in line:
-            print(line.replace(')',' "cuda")'))
-        else:
-            print(line)
 
 post_build = aur_post_build
 
