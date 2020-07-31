0351410dcbf64b43af60fff32563523f600f617b
Jingbei Li <i@jingbei.li>
2020-07-31 13:33:11 +0800

    fixed caffe2/lilac.py

--- caffe2/lilac.py
+++ caffe2/lilac.py
@@ -4,7 +4,7 @@ from lilaclib import *
 maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
 update_on = [{'aur': None}, {'alias': 'python'}]
 build_prefix = 'extra-x86_64'
-repo_depends = ['python-peachpy-git', 'python-scikit-image', 'python-lmdb', 'python-leveldb', 'python-glog', 'python-nvd3', 'python-slugify', 'rdma-core']
+repo_depends = ['python-scikit-image', 'python-nvd3']
 time_limit_hours = 3
 pre_build = aur_pre_build
 post_build = aur_post_build
