541899b53577999a45dd8956e4bb834981a96658
Jingbei Li <i@jingbei.li>
2020-07-31 13:28:02 +0800

    use actions to build python-wiringpi-git-*

--- python-wiringpi-git-aarch64/lilac.py
+++ python-wiringpi-git-aarch64/lilac.py
@@ -3,8 +3,7 @@ from lilaclib import *
 
 maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
 update_on = [{'aur': 'python-wiringpi-git'}, {'github': 'WiringPi/WiringPi-Python'}]
-build_prefix = 'extra-aarch64'
-makechrootpkg_args = ['-D', '/etc/ca-certificates/extracted']
+build_prefix = 'action-extra-aarch64'
 
 def pre_build():
     aur_pre_build('python-wiringpi-git')
@@ -13,4 +12,5 @@ def pre_build():
 post_build = aur_post_build
 
 if __name__ == '__main__':
-    single_main(build_prefix)
+    from action_tools import action_main
+    action_main(build_prefix)
--- python-wiringpi-git-armv7h/lilac.py
+++ python-wiringpi-git-armv7h/lilac.py
@@ -3,8 +3,7 @@ from lilaclib import *
 
 maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
 update_on = [{'aur': 'python-wiringpi-git'}, {'github': 'WiringPi/WiringPi-Python'}]
-build_prefix = 'extra-armv7h'
-makechrootpkg_args = ['-D', '/etc/ca-certificates/extracted']
+build_prefix = 'action-extra-armv7h'
 
 def pre_build():
     aur_pre_build('python-wiringpi-git')
@@ -12,4 +11,5 @@ def pre_build():
 post_build = aur_post_build
 
 if __name__ == '__main__':
-    single_main(build_prefix)
+    from action_tools import action_main
+    action_main(build_prefix)
