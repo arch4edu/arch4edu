d8a653d97f8f3b38804fab3d6feb9f75f8c3b3d0
Jingbei Li <i@jingbei.li>
2020-08-15 16:53:50 +0800

    added tensorflow-rocm for x86_64 #98

---
+++ tensorflow-rocm/lilac.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from lilaclib import *
+
+def pre_build():
+    aur_pre_build()
+
+    for line in edit_file('PKGBUILD'):
+        if line.startswith('_build_opt=1'):
+            pass
+        elif line.startswith('build() {')
+            print(line)
+            print('  export https_proxy=http://127.0.0.1:8123')
+        else:
+            print(line)
