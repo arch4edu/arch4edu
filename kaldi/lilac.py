505f0e592372b383deda44b5aa4e74698f3be5b4
Jingbei Li <i@jingbei.li>
2020-08-31 18:30:49 +0800

    fixed kaldi

--- kaldi/lilac.py
+++ kaldi/lilac.py
@@ -1,15 +1,2 @@
-#!/usr/bin/env python3
-from lilaclib import *
-
-maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
-update_on = [{'aur': None}, {'github': 'kaldi-asr/kaldi'}]
-build_prefix = 'extra-x86_64'
-repo_depends = ['cub', 'kaldi-openfst', 'openblas-lapack']
-
 def pre_build():
     aur_pre_build(do_vcs_update=True)
-
-post_build = aur_post_build
-
-if __name__ == '__main__':
-    single_main(build_prefix)
