4087b1730818e55a9195102d0c661fb976554469
Jingbei Li <i@jingbei.li>
2020-09-03 16:35:17 +0800

    fixed caffe2

--- caffe2/lilac.py
+++ caffe2/lilac.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python3
 from lilaclib import *
 
-maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
-update_on = [{'aur': None}, {'alias': 'python'}]
-build_prefix = 'extra-x86_64'
-repo_depends = ['python-scikit-image', 'python-nvd3']
-time_limit_hours = 3
-pre_build = aur_pre_build
-post_build = aur_post_build
-
-if __name__ == '__main__':
-    single_main(build_prefix)
+def pre_build():
+    aur_pre_build()
+    add_makedepends(['qt5-base'])
