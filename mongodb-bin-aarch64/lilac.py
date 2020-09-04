d025c1fe1c9516f3c1584cfffe452562326cfd1e
Jingbei Li <i@jingbei.li>
2020-09-04 17:31:05 +0800

    fixed mongodb-bin-aarch64

---
+++ mongodb-bin-aarch64/lilac.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from lilaclib import *
+
+maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
+update_on = [{'aur': 'mongodb-bin'}]
+build_prefix = 'action-extra-aarch64'
+
+def pre_build():
+    aur_pre_build('mongodb-bin')
+
+post_build = aur_post_build
+
+if __name__ == '__main__':
+    from action_tools import action_main
+    action_main(build_prefix)
