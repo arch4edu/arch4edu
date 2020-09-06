e818e3f36bae4cb2995ebc95cf18bba08f72fc42
Jingbei Li <i@jingbei.li>
2020-09-06 13:41:47 +0800

    fixed jabref

--- jabref/lilac.py
+++ jabref/lilac.py
@@ -4,7 +4,6 @@ from lilaclib import *
 maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
 update_on = [{'aur': None}]
 build_prefix = 'action-extra-x86_64'
-repo_depends = ['archlinux-java-run']
 pre_build = aur_pre_build
 post_build = aur_post_build
 
