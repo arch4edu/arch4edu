10453f06bb129c2672c8d10a76e003d1a8288689
Jingbei Li <i@jingbei.li>
2020-11-04 18:02:31 +0800

    fixed opencv-cuda

--- opencv-cuda/lilac.py
+++ opencv-cuda/lilac.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 from lilaclib import *
 
-maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
-build_prefix = 'arch4edu-x86_64'
-pre_build = aur_pre_build
-post_build = aur_post_build
-time_limit_hours = 4
+def pre_build():
+    aur_pre_build()
 
-if __name__ == '__main__':
-    single_main(build_prefix)
+    for line in edit_file('PKGBUILD'):
+        if line.startswith('build()'):
+            print('  export https_proxy=127.0.0.1:8123')
+        else:
+            print(line)
