29f2fd0602005b3dde3f56d90fd250631493208f
Jingbei Li <i@jingbei.li>
2020-12-15 15:14:42 +0800

    fixed libdart

---
+++ coin-or-ipopt/lilac.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from lilaclib import *
+
+def pre_build():
+    aur_pre_build()
+
+    for line in edit_file('PKGBUILD'):
+        if not line.startswith('groups=('):
+            print(line)
