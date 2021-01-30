4755260278c5d22eaf75586350ee738f93c4ca12
heavysink <winstonwu91@gmail.com>
2021-01-30 08:36:19 -0500

    Try trilinos update

---
+++ trilinos/lilac.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from lilaclib import *
+
+maintainers = [{'github': 'heavysink', 'email': 'Heavysink <winstonwu91@gmail.com>'}]
+build_prefix = 'extra-x86_64'
+repo_depends = ['libmatio']
+time_limit_hours = 8
+
+pre_build = aur_pre_build
+
+post_build = aur_post_build
+
+if __name__ == '__main__':
+    single_main(build_prefix)
