cb324dbf21eb894d1b573d16e50ef275edb39405
heavysink <winstonwu91@gmail.com>
2021-02-07 00:14:41 -0500

    [trilinos] Change netcdf dependency to netcdf-openmpi

--- trilinos/lilac.py
+++ trilinos/lilac.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 from lilaclib import *
 
-maintainers = [{'github': 'heavysink', 'email': 'Heavysink <winstonwu91@gmail.com>'}]
 build_prefix = 'extra-x86_64'
 repo_depends = ['libmatio']
 time_limit_hours = 8
 
-pre_build = aur_pre_build
+def pre_build():
+    aur_pre_build()
+    for line in edit_file('PKGBUILD'):
+        if 'depends=' in line:
+            print(line.replace('netcdf','netcdf-openmpi'))
+        else:
+            print(line)
 
 post_build = aur_post_build
 
