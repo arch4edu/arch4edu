d335ad614320ce1cb29976945e460a0509078dc5
Jingbei Li <i@jingbei.li>
2021-05-07 15:05:48 +0800

    use github actions to build yay

---
+++ yay/lilac.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from lilaclib import *
+
+maintainers = [{'github': 'petronny', 'email': 'Jingbei Li <i@jingbei.li>'}]
+build_prefix = 'action-extra-x86_64'
+pre_build = aur_pre_build
+post_build = aur_post_build
+
+if __name__ == '__main__':
+    from action_tools import action_main
+    action_main(build_prefix)
