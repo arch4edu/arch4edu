7118203b5402d1b7bfc616c38a8afbe56acc055c
cactus <cactus@jingbei.li>
2022-06-14 13:59:37 +0000

    visual-studio-code-bin: auto updated to 1.68.0-1

--- x86_64/visual-studio-code-bin/visual-studio-code-bin.sh
+++ x86_64/visual-studio-code-bin/visual-studio-code-bin.sh
@@ -4,7 +4,7 @@ XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-~/.config}
 
 # Allow users to override command-line options
 if [[ -f $XDG_CONFIG_HOME/code-flags.conf ]]; then
-   CODE_USER_FLAGS="$(sed 's/#.*//' $XDG_CONFIG_HOME/code-flags.conf)"
+   CODE_USER_FLAGS="$(sed 's/#.*//' $XDG_CONFIG_HOME/code-flags.conf | tr '\n' ' ')"
 fi
 
 # Launch
