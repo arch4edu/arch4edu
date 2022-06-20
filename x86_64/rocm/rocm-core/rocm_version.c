37bd3aabde67695983d0a6e8b105962df6cb33a3
Jingbei Li <i@jingbei.li>
2022-06-20 17:59:59 +0800

    added rocm-hip-runtime, moved rocm-opencl-runtime to cactus #141

---
+++ x86_64/rocm/rocm-core/rocm_version.c
@@ -0,0 +1,10 @@
+#include "rocm_version.h"
+
+VerErrors getROCmVersion(unsigned int *Major, unsigned int *Minor,
+                         unsigned int *Patch) {
+  *Major = ROCM_VERSION_MAJOR;
+  *Minor = ROCM_VERSION_MINOR;
+  *Patch = ROCM_VERSION_PATCH;
+
+  return 0;
+}
