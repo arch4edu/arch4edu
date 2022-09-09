a1bffb24e8018594b2bac59f998d6db3fdd63785
Jingbei Li <i@jingbei.li>
2022-09-09 13:41:45 +0800

    added help for smart-add-package.py

--- smart-add-package.py
+++ smart-add-package.py
@@ -15,8 +15,6 @@ remove = os.remove
 move = shutil.move
 rmtree = shutil.rmtree
 
-path = Path('.')
-
 def symlink(source, target):
     try:
         os.symlink(source, target)
@@ -35,7 +33,7 @@ def load_pacman_packages():
 
 def load_pkgbases():
     pkgbases = {}
-    for i in path.rglob('cactus.yaml'):
+    for i in Path('.').rglob('cactus.yaml'):
         pkgbase = i.parent.name
         pkgbases[pkgbase] = str(i.parent)
     return pkgbases
@@ -62,32 +60,48 @@ def read_provides(package):
         results[i] = package
     return results
 
-provides = {}
-provides.update(read_provides('r'))
-provides.update(read_provides('fuse2'))
-provides.update(read_provides('libjpeg-turbo'))
-provides.update(read_provides('ttf-dejavu'))
-provides.update(read_provides('ruby-ronn-ng'))
-provides.update(read_provides('libtool'))
-provides.update(read_provides('util-linux-libs'))
-provides.update(read_provides('pkgconf'))
-provides.update(read_provides('gtest'))
-provides.update(read_provides('dbus-python'))
-provides.update(read_provides('jdk-openjdk'))
-provides.update(read_provides('python-sip4'))
+def load_provides():
+    provides = {}
+    provides.update(read_provides('r'))
+    provides.update(read_provides('fuse2'))
+    provides.update(read_provides('libjpeg-turbo'))
+    provides.update(read_provides('ttf-dejavu'))
+    provides.update(read_provides('ruby-ronn-ng'))
+    provides.update(read_provides('libtool'))
+    provides.update(read_provides('util-linux-libs'))
+    provides.update(read_provides('pkgconf'))
+    provides.update(read_provides('gtest'))
+    provides.update(read_provides('dbus-python'))
+    provides.update(read_provides('jdk-openjdk'))
+    provides.update(read_provides('python-sip4'))
+    return provides
 
 if __name__ == '__main__':
+    import argparse
     from tornado.log import enable_pretty_logging
     from tornado.options import options
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--template', default='template/x86_64-simple.yaml', help='the template used to create cactus.yaml (default: template/x86_64-simple.yaml)')
+    #parser.add_argument('--repository', default='.', help='path to the repository (default: current directory)')
+    parser.add_argument('--nocheck', action="store_true", help='disable check and ignore checkdepends')
+    parser.add_argument('package', help='the package to add (eg: yay)')
+    parser.add_argument('directory', help='the output directory (eg: x86_64, x86_64/directory)')
+    args = parser.parse_args()
 
     options.logging = 'debug'
     logger = logging.getLogger()
     enable_pretty_logging(options=options, logger=logger)
 
+    directory = Path(args.directory)
+    directory.mkdir(exist_ok=True)
+    template = args.template
+
     pacman_db = load_pacman_packages()
     pkgbases = load_pkgbases()
+    provides = load_provides()
 
-    unresolved = [sys.argv[1]]
+    unresolved = [args.package]
     resolved = {}
     reversed_depends = {}
     while len(unresolved) > 0:
@@ -110,8 +124,8 @@ if __name__ == '__main__':
             _unresolved.update([i for i in aur_info[package]['Depends'] if not i in resolved])
 
             aur_info[package]['MakeDepends'] = [] if not 'MakeDepends' in aur_info[package] else aur_info[package]['MakeDepends']
-            #if 'CheckDepends' in aur_info[package]:
-            #        aur_info[package]['MakeDepends'] += aur_info[package]['CheckDepends']
+            if not args.nocheck and 'CheckDepends' in aur_info[package]:
+                aur_info[package]['MakeDepends'] += aur_info[package]['CheckDepends']
             aur_info[package]['MakeDepends'] = [i.split('>')[0].split('=')[0] for i in aur_info[package]['MakeDepends']]
             aur_info[package]['MakeDepends'] = [provides[i] if i in provides else i for i in aur_info[package]['MakeDepends']]
             aur_info[package]['MakeDepends'] = [i for i in aur_info[package]['MakeDepends'] if not i in pacman_db and not i in aur_info[package]['Depends']]
@@ -122,21 +136,16 @@ if __name__ == '__main__':
         unresolved = list(_unresolved)
     logger.info('Resolved %d packages', len(resolved))
 
-    category = Path(sys.argv[2])
-    category.mkdir(exist_ok=True)
-    template = 'template/x86_64-nocheck.yaml'
-
     for package, info in reversed(resolved.items()):
-
         pkgbase = info['PackageBase']
         if pkgbase in pkgbases:
             continue
 
-        pkgbase = category / pkgbase
+        pkgbase = directory / pkgbase
         pkgbase.mkdir(exist_ok=True)
 
         if len(info['Depends']) + len(info['MakeDepends']) == 0:
-            symlink('../' * (len(category.parents) + 1) + template, pkgbase / 'cactus.yaml')
+            symlink('../' * (len(directory.parents) + 1) + template, pkgbase / 'cactus.yaml')
             logger.info('Added %s with %s', package, template)
             pkgbases[pkgbase.name] = str(pkgbase)
         else:
@@ -149,7 +158,7 @@ if __name__ == '__main__':
                             f.write('depends:\n')
                         for i in info['Depends']:
                             _pkgbase = resolved[i]['PackageBase']
-                            _pkgbase = pkgbases[_pkgbase] if _pkgbase in pkgbases else str(category / _pkgbase)
+                            _pkgbase = pkgbases[_pkgbase] if _pkgbase in pkgbases else str(directory / _pkgbase)
                             if resolved[i]['PackageBase'] == i:
                                 f.write(f'  - {_pkgbase}\n')
                             else:
@@ -158,7 +167,7 @@ if __name__ == '__main__':
                             f.write('makedepends:\n')
                         for i in info['MakeDepends']:
                             _pkgbase = resolved[i]['PackageBase']
-                            _pkgbase = pkgbases[_pkgbase] if _pkgbase in pkgbases else str(category / _pkgbase)
+                            _pkgbase = pkgbases[_pkgbase] if _pkgbase in pkgbases else str(directory / _pkgbase)
                             if resolved[i]['PackageBase'] == i:
                                 f.write(f'  - {_pkgbase}\n')
                             else:
