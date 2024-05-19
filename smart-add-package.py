8c4c59f10c265332d6f86d83bff1213218d5939d
Carlos Aznarán <caznaranl@uni.pe>
2024-05-19 14:42:31 -0500

    Replace dbus-python by python-dbus

--- smart-add-package.py
+++ smart-add-package.py
@@ -70,7 +70,7 @@ def load_provides():
     provides.update(read_provides('util-linux-libs'))
     provides.update(read_provides('pkgconf'))
     provides.update(read_provides('gtest'))
-    provides.update(read_provides('dbus-python'))
+    provides.update(read_provides('python-dbus'))
     provides.update(read_provides('jdk-openjdk'))
     provides.update(read_provides('jre-openjdk'))
     return provides
